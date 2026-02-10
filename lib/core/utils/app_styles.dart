3892484c299f1ea696ba93f31c5fb9569a502292
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-10 23:40:18 +0200

    refactor: adding application text styles

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppStyles {
+  static final headLine = GoogleFonts.poppins(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+  static final sectionTitle = GoogleFonts.poppins(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  static final cardTitle = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static final bodyText = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static final secondary = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static final caption = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+}
