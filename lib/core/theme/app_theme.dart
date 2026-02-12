b87d4bd796fd592b83c7f91e976f381338c8127c
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-12 02:10:47 +0200

    chore: collect app themes in a file

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:stay_match/core/constants/app_colors.dart';
+
+abstract class AppTheme {
+  static final theme = ThemeData(
+    scaffoldBackgroundColor: AppColors.primary
+  );
+}
