5193bb90ff9e4b9faf8056fef57fd2a8a9248035
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-12 03:11:13 +0200

    chore: add more login page strings

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,9 +1,15 @@
 abstract class AppStrings {
   // Login screen
+  static const email = 'Email';
   static const enterYourEmail = 'Enter your Email';
+  static const password = 'Password';
   static const enterYourPassword = 'Enter your password';
   static const forgetPassword = 'Forget Password ?';
-  static const newPassword = 'New Password ?'; // wth is that??
+  static const newPassword = 'New Password ?'; // todo: wth is that??
+  static const or = 'OR';
+  static const login = 'Login';
+  static const loginWithGoogle = 'Login with Google';
+  static const signUp = 'Sign Up !';
 
   // Signup screen
 }
