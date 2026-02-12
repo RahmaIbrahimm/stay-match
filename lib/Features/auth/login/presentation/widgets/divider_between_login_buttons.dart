1f131d5b7e2fb94d764bb6a0f4fd9017befd82b4
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-13 00:22:06 +0200

    feat: add DividerBetweenLoginButtons widget for improved layout between login buttons

---
+++ lib/Features/auth/login/presentation/widgets/divider_between_login_buttons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/constants/app_strings.dart';
+import '../../../../../core/constants/app_styles.dart';
+
+class DividerBetweenLoginButtons extends StatelessWidget {
+  const DividerBetweenLoginButtons({super.key, required this.size});
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Divider(
+            color: Colors.black,
+            thickness: 1,
+            indent: size.width * 0.08,
+            endIndent: 10,
+          ),
+        ),
+        Text(AppStrings.or, style: AppStyles.cardTitle),
+        Expanded(
+          child: Divider(
+            color: Colors.black,
+            thickness: 1,
+            endIndent: size.width * 0.08,
+            indent: 10,
+          ),
+        ),
+      ],
+    );
+  }
+}
