13ec5f87100372bc6882b053cffbd7a753aed999
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-12 03:07:52 +0200

    feat: form section done for login bottom sheet

--- lib/Features/auth/login/presentation/widgets/login_view_body_bottom_sheet.dart
+++ lib/Features/auth/login/presentation/widgets/login_view_body_bottom_sheet.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:stay_match/core/constants/app_strings.dart';
 
-import '../../../../../core/widgets/custom_text_form_field.dart';
+import '../../../widgets/form_section.dart';
 
 class LoginViewBodyBottomSheet extends StatelessWidget {
   const LoginViewBodyBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomTextFormField(hintText: 'dfd')]);
+    return Column(
+      children: [
+        SizedBox(height: 33),
+        FormSection(
+          validator: (value) {},
+          hintText:AppStrings.enterYourEmail ,
+          fieldTitle:  AppStrings.email,
+        ),
+        SizedBox(height: 37,),
+        FormSection(
+          validator: (value) {},
+          hintText: AppStrings.enterYourPassword ,
+          fieldTitle:AppStrings.password ,
+        ),
+      ],
+    );
   }
 }
