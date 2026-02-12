fa0efd764eccff532fa9e9ee7ae586357dba8846
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-12 02:12:41 +0200

    feat: add login view body bottom sheet widget

---
+++ lib/Features/auth/login/presentation/widgets/login_view_body_bottom_sheet.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/widgets/custom_text_form_field.dart';
+
+class LoginViewBodyBottomSheet extends StatelessWidget {
+  const LoginViewBodyBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [CustomTextFormField(hintText: 'dfd')]);
+  }
+}
