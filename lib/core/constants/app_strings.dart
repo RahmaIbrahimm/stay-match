c478c7ec0be3c88bbaa812a3db255963c57c4dc4
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-13 19:11:48 +0200

    chore: update strings for forget password screen

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -17,16 +17,22 @@ abstract class AppStrings {
 
   // Login screen
   static const signUpGreeting = 'Welcome To StayMatch';
-  static const forgetPassword = 'Forget Password ?';
-  static const newPassword = 'New Password ?'; // todo: wth is that??
+  static const forgetPasswordQuestion = 'Forget Password ?';
   static const or = 'OR';
   static const loginWithGoogle = 'Login with Google';
   static const dontHaveAnAccount = 'Don\'t Have an Account?';
   static const signUp = 'Sign Up !';
-
   // Sign up screen
+
   static const signUpTitle = 'Sign Up';
   static const submit = 'Submit';
   static const loginGreeting = 'Welcome Back To StayMatch';
   static const alreadyHaveAnAccount = 'Already Have an Accound?';
+
+  // forget password screen
+  static const emailAddress = 'Email Address';
+  static const confirmEmail = 'Confirm Email';
+  static const newPassword = 'New Password ?'; // todo: wth is that??
+  static const forgetPassword = 'Forget Password';
+  static const forgetPasswordInstructions = 'Please Write your Email to receive a\nconfirmation code to sent a new password ';
 }
