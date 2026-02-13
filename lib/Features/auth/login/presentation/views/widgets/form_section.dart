0d605927d71bd4a5b7bcbe56d8d3ab0f1c576941
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-13 17:57:32 +0200

    feat: add bottom sheet body for signup form with validation sections

--- lib/Features/auth/login/presentation/views/widgets/form_section.dart
+++ lib/Features/auth/login/presentation/views/widgets/form_section.dart
@@ -4,21 +4,30 @@ import '../../../../../../core/constants/app_styles.dart';
 import '../../../../../../core/widgets/custom_text_form_field.dart';
 
 class FormSection extends StatelessWidget {
-  const FormSection(
-      {super.key, required this.validator, required this.hintText, required this.fieldTitle});
+  const FormSection({
+    super.key,
+    required this.validator,
+    required this.hintText,
+    required this.fieldTitle,
+    this.suffixIcon,
+  });
 
   final String? Function(String?) validator;
   final String hintText;
   final String fieldTitle;
+  final Widget? suffixIcon;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         //Todo: add validator
-        Text(fieldTitle, style: AppStyles.sectionTitle,),
+        Text(fieldTitle, style: AppStyles.sectionTitle),
         CustomTextFormField(
-            hintText: hintText, validator: validator)
+          hintText: hintText,
+          validator: validator,
+          suffixIcon: suffixIcon,
+        ),
       ],
     );
   }
---
+++ lib/Features/auth/signup/presentation/views/widgets/signup_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart%20';
+import 'package:stay_match/features/auth/signup/presentation/views/widgets/signup_header.dart';
+import 'package:stay_match/features/auth/widgets/custom_bottom_sheet.dart';
+
+import 'bottom_sheet_body.dart';
+
+class SignUpViewBody extends StatelessWidget {
+  const SignUpViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(height: 45),
+        Padding(padding: const EdgeInsets.all(16.0), child: SignUpHeader()),
+        const SizedBox(height: 20),
+        Expanded(child: CustomBottomSheet(sheetBody: const ButtomSheetBody())),
+      ],
+    );
+  }
+}
