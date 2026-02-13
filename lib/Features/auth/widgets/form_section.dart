58090bf3dbf48b42d467c464a51855e0262ae68a
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-13 03:42:08 +0200

    feat: add suffixIcon parameter to FormSection

--- lib/Features/auth/widgets/form_section.dart
+++ lib/Features/auth/widgets/form_section.dart
@@ -4,12 +4,12 @@ import '../../../core/constants/app_styles.dart';
 import '../../../core/widgets/custom_text_form_field.dart';
 class FormSection extends StatelessWidget {
   const FormSection(
-      {super.key, required this.validator, required this.hintText, required this.fieldTitle});
+      {super.key, required this.validator, required this.hintText, required this.fieldTitle, this.suffixIcon});
 
   final String? Function(String?) validator;
   final String hintText;
   final String fieldTitle;
-
+final Widget? suffixIcon;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -19,7 +19,7 @@ class FormSection extends StatelessWidget {
         Text(fieldTitle, style: AppStyles.sectionTitle,),
         SizedBox(height: 7,),
         CustomTextFormField(
-            hintText: hintText, validator: validator)
+            hintText: hintText, validator: validator,suffixIcon: suffixIcon,)
       ],
     );
   }
