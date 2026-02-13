7114d43e65e89070b4c8ec64bdf980aaddbe1fb4
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-13 19:11:32 +0200

    feat: add forget password background image path

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,6 +1,10 @@
 abstract class AppImages {
   static const path = 'assets/images';
+  // login page
   static const loginDecor = '$path/login_decor.png';
   static const loginDecorRight = '$path/login_decor_right.png';
   static const loginDecorLeft = '$path/login_decor_left.png';
+
+  // forget password
+  static const forgetPasswordBg = '$path/Forget password_bg.png';
 }
