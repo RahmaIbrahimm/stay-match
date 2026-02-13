8bed7b03450e78f98ace02302e08ca5381437b34
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-13 16:53:28 +0200

    feat: update app_strings for signup

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,17 +1,32 @@
 abstract class AppStrings {
-  // Login screen
+  // auth form
   static const email = 'Email';
-  static const welcomeBack = 'Welcome Back To StayMatch';
   static const enterYourEmail = 'Enter your Email';
   static const password = 'Password';
   static const enterYourPassword = 'Enter your password';
+  static const gender = 'Gender';
+  static const enterYourGender= 'Enter your gender';
+  static const name = 'Name';
+  static const enterYourName = 'Enter your name';
+  static const location = 'Location';
+  static const enterYourLocation = 'Enter your location';
+  static const age = 'Age';
+  static const enterYourAge = 'Enter your age';
+  static const confirmPassword = 'Confirm Password';
+  static const login = 'Login';
+
+  // Login screen
+  static const signUpGreeting = 'Welcome To StayMatch';
   static const forgetPassword = 'Forget Password ?';
   static const newPassword = 'New Password ?'; // todo: wth is that??
   static const or = 'OR';
-  static const login = 'Login';
   static const loginWithGoogle = 'Login with Google';
-  static const dontHaveAnAccount = 'Don\'t have an Account?';
-  static const signUp = 'Sign Up!';
+  static const dontHaveAnAccount = 'Don\'t Have an Account?';
+  static const signUp = 'Sign Up !';
 
-  // Signup screen
+  // Sign up screen
+  static const signUpTitle = 'Sign Up';
+  static const submit = 'Submit';
+  static const loginGreeting = 'Welcome Back To StayMatch';
+  static const alreadyHaveAnAccount = 'Already Have an Accound?';
 }
