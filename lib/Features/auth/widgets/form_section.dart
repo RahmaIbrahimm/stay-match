46d3d7995ffa6efe2381c6fd47d43b5e7d84c928
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-21 01:05:41 +0200

    feat: add controller parameter to form section for improved input handling

--- lib/Features/auth/widgets/form_section.dart
+++ lib/Features/auth/widgets/form_section.dart
@@ -9,13 +9,14 @@ class FormSection extends StatelessWidget {
     required this.validator,
     required this.hintText,
     required this.fieldTitle,
-     this.suffixIcon,
+    this.suffixIcon, required this.controller,
   });
 
   final String? Function(String?) validator;
   final String hintText;
   final String fieldTitle;
   final Widget? suffixIcon;
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +26,12 @@ class FormSection extends StatelessWidget {
         //Todo: add validator
         Text(fieldTitle, style: AppStyles.sectionTitle),
         const SizedBox(height: 7),
-        CustomTextFormField(hintText: hintText, validator: validator,suffixIcon: suffixIcon,),
+        CustomTextFormField(
+          hintText: hintText,
+          validator: validator,
+          suffixIcon: suffixIcon,
+          controller: controller,
+        ),
       ],
     );
   }
