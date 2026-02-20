dfde0be3175f2e822197042dbd5d7bc7d2d0998a
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-21 01:01:19 +0200

    feat: add NewPasswordViewBody and refactor VerifyEmailContainerBody for improved UI structure

--- lib/Features/auth/verify_email/presentation/views/widgets/verify_email_container_body.dart
+++ lib/Features/auth/verify_email/presentation/views/widgets/verify_email_container_body.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pinput/pinput.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 import 'package:stay_match/core/constants/app_strings.dart';
 import 'package:stay_match/core/constants/app_styles.dart';
 import 'package:stay_match/core/widgets/custom_elevated_button.dart';
-import 'package:stay_match/features/auth/verify_email/presentation/views/widgets/verifiction_numbers.dart';
+import 'package:stay_match/features/auth/verify_email/presentation/views/widgets/verify_email_otp.dart';
 
 class VerifyEmailContainerBody extends StatelessWidget {
   const VerifyEmailContainerBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisSize: MainAxisSize.min,
+    return ListView(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      clipBehavior: Clip.antiAlias,
       children: [
         Text(
           AppStrings.verifyEmailAddress,
@@ -36,19 +38,29 @@ class VerifyEmailContainerBody extends StatelessWidget {
         ),
         Padding(
           padding: const EdgeInsets.all(20),
-          child: VerificationNumbers(),
+          child: VerifyEmailOTP(validator: (String? p1) {  },)
         ),
-        CustomElevatedButton(text: AppStrings.confirmCode, onPressed: (){}),
-        const SizedBox(height: 16 ,),
+        CustomElevatedButton(text: AppStrings.confirmCode, onPressed: () {}),
+        const SizedBox(height: 16),
         // todo: timer implementation
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           spacing: 5,
           children: [
-            Text('0:29',style: AppStyles.bodyText.copyWith(color: AppColors.textColorPrimary)),
-            Text(AppStrings.resendConfirmation,style: AppStyles.bodyText.copyWith(color: AppColors.textColorSecondary),),
+            Text(
+              '0:29',
+              style: AppStyles.bodyText.copyWith(
+                color: AppColors.textColorPrimary,
+              ),
+            ),
+            Text(
+              AppStrings.resendConfirmation,
+              style: AppStyles.bodyText.copyWith(
+                color: AppColors.textColorSecondary,
+              ),
+            ),
           ],
-        )
+        ),
       ],
     );
   }
