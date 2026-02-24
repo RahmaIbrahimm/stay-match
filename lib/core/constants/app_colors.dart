7f9d2b1ba6aaae554e22316960b58c5254d765ae
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-25 01:40:53 +0200

    feat: add boxShadow constant for improved UI styling

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -6,11 +6,20 @@ abstract class AppColors {
   static const accentColor = Color(0xff4D84C8);
   static const backgroundColor = Colors.black;
   static const containerColor = Colors.white;
-  static const  textColorWhite= Color(0xffffffff);
-  static const  textColorPrimary= Color(0xff19212C);
-  static const textColorSecondary= Color(0xff565B66);
-  static const textColorError= Color(0xffBF3636);
-  static const textColorSuccess= Color(0xff0D9467);
+  static const textColorWhite = Color(0xffffffff);
+  static const textColorPrimary = Color(0xff19212C);
+  static const textColorSecondary = Color(0xff565B66);
+  static const textColorError = Color(0xffBF3636);
+  static const textColorSuccess = Color(0xff0D9467);
   static const shadowColor = Color(0x33000000);
   static const grey = Color(0xffD9D9D9);
+
+  static const boxShadow = [
+    BoxShadow(
+      color: AppColors.shadowColor,
+      offset: Offset(5, 5),
+      blurRadius: 5,
+      spreadRadius: 2,
+    ),
+  ];
 }
