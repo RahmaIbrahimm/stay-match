337d7dcffb8f7ff8668d48c25eb0b9ef908876ee
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-25 20:01:02 +0200

    feat: integrate LoginCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stay_match/core/theme/app_theme.dart';
-import 'features/auth/login/presentation/views/login_view.dart';
+import 'package:stay_match/features/auth/signup/presentation/views/signup_view.dart';
+
+import 'features/auth/login/presentation/manager/login_cubit.dart';
 
 void main() {
   runApp(StayMatch());
 }
+
 class StayMatch extends StatelessWidget {
   const StayMatch({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.theme,
-      home: LoginView(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => LoginCubit())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.theme,
+        home: SignUpView(),
+      ),
     );
   }
 }
