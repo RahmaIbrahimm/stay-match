ec47090bf5cfe6d616bc5e1cae803c9bf4a78a7f
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-28 01:58:53 +0200

    feat: refactor AppRouting to use static router for improved routing management

--- lib/core/routing/app_routing.dart
+++ lib/core/routing/app_routing.dart
@@ -2,11 +2,11 @@ import 'package:go_router/go_router.dart';
 import 'package:stay_match/features/auth/login/presentation/views/login_view.dart';
 import 'package:stay_match/features/auth/signup/presentation/views/signup_view.dart';
 
-abstract class AppRouting {
+class AppRouting {
   static const loginView ='/';
   static const signupView ='/signupView';
 
-  final router = GoRouter(
+  static final router = GoRouter(
   routes: [
     GoRoute(path: loginView, builder: (context, state) => const LoginView(),),
     GoRoute(path: signupView,builder: (context, state) => const SignUpView(),),
