318e4ccc0610da32bbfc46ca7f773da61515057a
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 01:26:32 +0200

    feat: update VerifyCodeResponse to include data field for userId

--- lib/Features/auth/data/models/verify_code_response.dart
+++ lib/Features/auth/data/models/verify_code_response.dart
@@ -2,45 +2,47 @@
 /// message : "Verification failed"
 /// errors : {"InvalidCode":["Invalid code."]}
 
-class VerifyEmailResponse {
-  VerifyEmailResponse({
-      this.isSuccess, 
-      this.message, 
-      this.errors,});
+class VerifyCodeResponse {
+  VerifyCodeResponse({this.isSuccess, this.message, this.errors});
 
-  VerifyEmailResponse.fromJson(dynamic json) {
+  VerifyCodeResponse.fromJson(dynamic json) {
     isSuccess = json['isSuccess'];
     message = json['message'];
     errors = json['errors'] != null ? Errors.fromJson(json['errors']) : null;
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
   }
+
   bool? isSuccess;
   String? message;
   Errors? errors;
-
+  Data? data;
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['isSuccess'] = isSuccess;
+    map['data'] = data;
     map['message'] = message;
     if (errors != null) {
       map['errors'] = errors?.toJson();
     }
+    if (data != null) {
+      map['data'] = data?.toJson();
+    }
     return map;
   }
-
 }
 
 /// InvalidCode : ["Invalid code."]
 /// "Email": ["The Email field is not a valid e-mail address."]
 class Errors {
-  Errors({
-      this.invalidCode,
-      this.email,
-  });
+  Errors({this.invalidCode, this.email});
 
   Errors.fromJson(dynamic json) {
-    invalidCode = json['InvalidCode'] != null ? json['InvalidCode'].cast<String>() : [];
+    invalidCode = json['InvalidCode'] != null
+        ? json['InvalidCode'].cast<String>()
+        : [];
     email = json['Email'] != null ? json['Email'].cast<String>() : [];
   }
+
   List<String>? invalidCode;
   List<String>? email;
 
@@ -50,5 +52,19 @@ class Errors {
     map['Email'] = email;
     return map;
   }
+}
 
+class Data {
+  String? userId;
+
+  Data({required this.userId});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+  }
+  Map<String,dynamic> toJson() {
+    return {
+      'userId' : userId
+    };
+  }
 }
