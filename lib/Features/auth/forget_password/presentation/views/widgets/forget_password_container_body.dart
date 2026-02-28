3f4bb1598ec811008b05f23489edf10a354212d5
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-28 17:15:40 +0200

    feat: integrate AuthCubit for form handling in forget password container

--- lib/Features/auth/forget_password/presentation/views/widgets/forget_password_container_body.dart
+++ lib/Features/auth/forget_password/presentation/views/widgets/forget_password_container_body.dart
@@ -1,49 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:stay_match/core/constants/app_strings.dart';
 import 'package:stay_match/core/constants/app_styles.dart';
+import 'package:stay_match/core/routing/app_routing.dart';
 import 'package:stay_match/core/widgets/custom_elevated_button.dart';
+import 'package:stay_match/features/auth/data/manager/auth_cubit.dart';
 import 'package:stay_match/features/auth/login/presentation/views/widgets/form_section.dart';
 
 class ForgetPasswordContainerBody extends StatelessWidget {
   ForgetPasswordContainerBody({super.key});
 
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
 
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: const BouncingScrollPhysics(),
-      children: [
-        Text(
-          AppStrings.forgetPassword,
-          style: AppStyles.headLine,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 16),
-        Text(
-          AppStrings.forgetPasswordInstructions,
-          style: AppStyles.bodyText,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 24),
-        // todo implement validation
-        Form(
-          key: formKey,
-          child: FormSection(
+    var authCubit = BlocProvider.of<AuthCubit>(context);
+    return Form(
+      key: authCubit.forgetFormKey,
+      child: ListView(
+        shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
+        children: [
+          Text(
+            AppStrings.forgetPassword,
+            style: AppStyles.headLine,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            AppStrings.forgetPasswordInstructions,
+            style: AppStyles.bodyText,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 24),
+          // todo implement validation
+          FormSection(
             validator: (val) {},
             hintText: AppStrings.enterYourEmail,
             fieldTitle: AppStrings.emailAddress,
             stroke: false,
-            controller: _emailController,
+            controller: authCubit.forgetEmailController ,
           ),
-        ),
-        const SizedBox(height: 36),
-        // todo implement on pressed logic
-        CustomElevatedButton(text: AppStrings.confirmEmail, onPressed: () {}),
-      ],
+          const SizedBox(height: 36),
+          // todo implement on pressed logic
+          CustomElevatedButton(text: AppStrings.confirmEmail, onPressed: () {
+            authCubit.emailForForgetPassValidation();
+            context.push(AppRouting.verifyEmailView);
+          }),
+        ],
+      ),
     );
   }
 }
