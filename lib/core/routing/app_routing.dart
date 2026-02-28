b1f74344dd06640cca7f40048ba3ea1ffbcb9de0
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-28 17:11:41 +0200

    feat: add routes for ForgetPasswordView and VerifyEmailView in AppRouting

--- lib/core/routing/app_routing.dart
+++ lib/core/routing/app_routing.dart
@@ -1,15 +1,21 @@
 import 'package:go_router/go_router.dart';
 import 'package:stay_match/features/auth/login/presentation/views/login_view.dart';
 import 'package:stay_match/features/auth/signup/presentation/views/signup_view.dart';
+import '../../features/auth/forget_password/presentation/views/forget_password_view.dart';
+import '../../features/auth/verify_email/presentation/views/verify_email_view.dart';
 
 class AppRouting {
   static const loginView ='/';
   static const signupView ='/signupView';
+  static const forgetPasswordView ='/forgetPasswordView';
+  static const verifyEmailView ='/verifyEmailView';
 
   static final router = GoRouter(
   routes: [
     GoRoute(path: loginView, builder: (context, state) => const LoginView(),),
     GoRoute(path: signupView,builder: (context, state) => const SignUpView(),),
+    GoRoute(path: forgetPasswordView,builder: (context, state) => const ForgetPasswordView(),),
+    GoRoute(path: verifyEmailView,builder: (context, state) => const VerifyEmailView(),),
   ]
   );
 }
