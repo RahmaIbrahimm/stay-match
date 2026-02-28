cc2864ac0ffe40f41b6b5db891f4834fd770fd8a
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 01:25:33 +0200

    feat: add dependencies for Bloc and Auth management in VerifyEmailView

--- lib/Features/auth/verify_email/presentation/views/verify_email_view.dart
+++ lib/Features/auth/verify_email/presentation/views/verify_email_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stay_match/core/utils/service_locator.dart';
+import 'package:stay_match/features/auth/data/manager/auth_cubit.dart';
+import 'package:stay_match/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:stay_match/features/auth/verify_email/presentation/views/widgets/verify_email_view_body.dart';
 
 class VerifyEmailView extends StatelessWidget {
@@ -6,6 +10,8 @@ class VerifyEmailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: const Scaffold(body: VerifyEmailViewBody()));
+    return SafeArea(
+      child: const Scaffold(body: VerifyEmailViewBody()),
+    );
   }
 }
