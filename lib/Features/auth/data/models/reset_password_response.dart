72fbc830e2c7b0e3a82278f059b92785b86e6034
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 01:31:54 +0200

    feat: implement reset password functionality with UI and state management and change names

---
+++ lib/Features/auth/data/models/reset_password_response.dart
@@ -0,0 +1,69 @@
+/// isSuccess : false
+/// message : "Password reset failed"
+/// errors : {"PasswordTooShort":["Passwords must be at least 6 characters."],"PasswordRequiresNonAlphanumeric":["Passwords must have at least one non alphanumeric character."],"PasswordRequiresDigit":["Passwords must have at least one digit ('0'-'9')."],"InvalidUser":["Something went wrong."],"ConfirmPassword":["Passwords do not match."]}
+
+class ResetPasswordResponse {
+  ResetPasswordResponse({
+      this.isSuccess, 
+      this.message, 
+      this.errors,});
+
+  ResetPasswordResponse.fromJson(dynamic json) {
+    isSuccess = json['isSuccess'];
+    message = json['message'];
+    errors = json['errors'] != null ? Errors.fromJson(json['errors']) : null;
+  }
+  bool? isSuccess;
+  String? message;
+  Errors? errors;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['isSuccess'] = isSuccess;
+    map['message'] = message;
+    if (errors != null) {
+      map['errors'] = errors?.toJson();
+    }
+    return map;
+  }
+
+}
+
+/// PasswordTooShort : ["Passwords must be at least 6 characters."]
+/// PasswordRequiresNonAlphanumeric : ["Passwords must have at least one non alphanumeric character."]
+/// PasswordRequiresDigit : ["Passwords must have at least one digit ('0'-'9')."]
+/// InvalidUser : ["Something went wrong."]
+/// ConfirmPassword : ["Passwords do not match."]
+
+class Errors {
+  Errors({
+      this.passwordTooShort, 
+      this.passwordRequiresNonAlphanumeric, 
+      this.passwordRequiresDigit, 
+      this.invalidUser, 
+      this.confirmPassword,});
+
+  Errors.fromJson(dynamic json) {
+    passwordTooShort = json['PasswordTooShort'] != null ? json['PasswordTooShort'].cast<String>() : [];
+    passwordRequiresNonAlphanumeric = json['PasswordRequiresNonAlphanumeric'] != null ? json['PasswordRequiresNonAlphanumeric'].cast<String>() : [];
+    passwordRequiresDigit = json['PasswordRequiresDigit'] != null ? json['PasswordRequiresDigit'].cast<String>() : [];
+    invalidUser = json['InvalidUser'] != null ? json['InvalidUser'].cast<String>() : [];
+    confirmPassword = json['ConfirmPassword'] != null ? json['ConfirmPassword'].cast<String>() : [];
+  }
+  List<String>? passwordTooShort;
+  List<String>? passwordRequiresNonAlphanumeric;
+  List<String>? passwordRequiresDigit;
+  List<String>? invalidUser;
+  List<String>? confirmPassword;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['PasswordTooShort'] = passwordTooShort;
+    map['PasswordRequiresNonAlphanumeric'] = passwordRequiresNonAlphanumeric;
+    map['PasswordRequiresDigit'] = passwordRequiresDigit;
+    map['InvalidUser'] = invalidUser;
+    map['ConfirmPassword'] = confirmPassword;
+    return map;
+  }
+
+}
---
+++ lib/Features/auth/new_password/presentation/views/reset_password_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stay_match/core/utils/service_locator.dart';
+import 'package:stay_match/features/auth/data/manager/auth_cubit.dart';
+import 'package:stay_match/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:stay_match/features/auth/new_password/presentation/views/widgets/reset_password_view_Body.dart';
+
+class ResetPasswordView extends StatelessWidget {
+  const ResetPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(body: ResetPasswordViewBody()));
+  }
+}
