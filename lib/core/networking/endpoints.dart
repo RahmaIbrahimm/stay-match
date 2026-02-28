4377a5bb103a08e1684d084f8f25b11c0a7b12c4
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 01:28:57 +0200

    feat: add endpoints for code verification and password reset

--- lib/core/networking/endpoints.dart
+++ lib/core/networking/endpoints.dart
@@ -2,4 +2,6 @@ abstract class Endpoints {
   static String baseUrl = 'https://graduationproject1.runasp.net/';
   static String login = 'login';
   static String forgetPassword = 'api/Account/forgot-password';
+  static String verifyCode = 'api/Account/verify-code';
+  static String resetPassword = 'api/Account/reset-password';
 }
