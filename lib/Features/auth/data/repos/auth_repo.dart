7f03f4eea1c8c6f4e2fcd20cf665b15076b9fde0
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 01:27:56 +0200

    feat: update AuthRepo to include methods for OTP verification and password reset

--- lib/Features/auth/data/repos/auth_repo.dart
+++ lib/Features/auth/data/repos/auth_repo.dart
@@ -1,8 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:stay_match/core/errors/failures.dart';
 import 'package:stay_match/features/auth/data/models/forget_password_response.dart';
+import 'package:stay_match/features/auth/data/models/reset_password_response.dart';
 
 import '../models/login_response.dart';
+import '../models/verify_code_response.dart';
 
 abstract class AuthRepo {
   Future<Either<Failure, LoginResponse>> login({
@@ -14,6 +16,12 @@ abstract class AuthRepo {
   String? emailValidator({String? email});
 
   String? passwordValidator({String? password});
+
   // ========= forget password =========
-  Future<Either<Failure,ForgetPasswordResponse>> sendVerificationCode({String? email});
+  Future<Either<Failure, ForgetPasswordResponse>> sendVerificationCode({
+    required String email,
+  });
+  // ========== confirm password ========
+  Future<Either<Failure,VerifyCodeResponse>> verifyOTP({required String code,required String email});
+  Future<Either<Failure,ResetPasswordResponse>> resetPassword({required String password,required String confirmPassword,required String userId});
 }
