fc9c4b0c3bf983617e005b17575f144253c226d2
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 01:24:59 +0200

    feat: implement BlocConsumer for OTP verification and handle success/failure states

--- lib/Features/auth/verify_email/presentation/views/widgets/verify_email_container_body.dart
+++ lib/Features/auth/verify_email/presentation/views/widgets/verify_email_container_body.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:pinput/pinput.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 import 'package:stay_match/core/constants/app_strings.dart';
 import 'package:stay_match/core/constants/app_styles.dart';
+import 'package:stay_match/core/routing/app_routing.dart';
 import 'package:stay_match/core/widgets/custom_elevated_button.dart';
+import 'package:stay_match/features/auth/data/manager/auth_cubit.dart';
 import 'package:stay_match/features/auth/verify_email/presentation/views/widgets/verify_email_otp.dart';
 
 class VerifyEmailContainerBody extends StatelessWidget {
@@ -12,56 +14,111 @@ class VerifyEmailContainerBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.zero,
-      shrinkWrap: true,
-      clipBehavior: Clip.antiAlias,
-      children: [
-        Text(
-          AppStrings.verifyEmailAddress,
-          style: AppStyles.headLine,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 16),
-        Text(
-          AppStrings.verificationCodeSentTo,
-          style: AppStyles.cardTitle.copyWith(
-            color: AppColors.textColorSecondary,
+    var authCubit = BlocProvider.of<AuthCubit>(context);
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is VerifyCodeStateFailure) {
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              backgroundColor: Colors.red[800],
+              content: Text(state.errMessage),
+            ),
+          );
+        }
+        if (state is VerifyCodeStateSuccess) {
+          final userId = state.response.data?.userId;
+          authCubit.otpController.clear();
+          if (userId != null) {
+            context.push(
+              AppRouting.newPasswordView,
+              extra: userId,
+            );}
+          else{
+            print('problem arised $userId userid');
+          }
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              backgroundColor: Colors.green,
+              content: Text(state.response.message ?? 'SUCCESS'),
+            ),
+          );
+          context.go(AppRouting.newPasswordView);
+        }
+      },
+      builder: (context, state) {
+        return Form(
+          key: authCubit.verifyCodeFormKey,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            clipBehavior: Clip.antiAlias,
+            children: [
+              Text(
+                AppStrings.verifyEmailAddress,
+                style: AppStyles.headLine,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                AppStrings.verificationCodeSentTo,
+                style: AppStyles.cardTitle.copyWith(
+                  color: AppColors.textColorSecondary,
+                ),
+              ),
+              // TODO: implement logic Email sent to
+              Text(
+                authCubit.forgetEmailController.text,
+                style: AppStyles.cardTitle.copyWith(color: AppColors.secondary),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: VerifyEmailOTP(
+                  validator: (String? p1) {
+                    if (p1 == null || p1.isEmpty) {
+                      return 'Code is required';
+                    }
+                    return null;
+                  },
+                  controller: authCubit.otpController,
+                ),
+              ),
+              CustomElevatedButton(
+                text: AppStrings.confirmCode,
+                onPressed: () {
+                  if (authCubit.verifyCodeFormKey.currentState!.validate()) {
+                    authCubit.verifyOTP();
+                    context.pop();
+                  }
+                },
+              ),
+              const SizedBox(height: 16),
+              // todo: timer implementation
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 5,
+                children: [
+                  Text(
+                    '0:29',
+                    style: AppStyles.bodyText.copyWith(
+                      color: AppColors.textColorPrimary,
+                    ),
+                  ),
+                  Text(
+                    AppStrings.resendConfirmation,
+                    style: AppStyles.bodyText.copyWith(
+                      color: AppColors.textColorSecondary,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ),
-        // TODO: implement logic Email sent to
-        Text(
-          'person@gmail.com',
-          style: AppStyles.cardTitle.copyWith(color: AppColors.secondary),
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: VerifyEmailOTP(validator: (String? p1) {  },)
-        ),
-        CustomElevatedButton(text: AppStrings.confirmCode, onPressed: () {}),
-        const SizedBox(height: 16),
-        // todo: timer implementation
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 5,
-          children: [
-            Text(
-              '0:29',
-              style: AppStyles.bodyText.copyWith(
-                color: AppColors.textColorPrimary,
-              ),
-            ),
-            Text(
-              AppStrings.resendConfirmation,
-              style: AppStyles.bodyText.copyWith(
-                color: AppColors.textColorSecondary,
-              ),
-            ),
-          ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
