88c5cbc93d8f3783aabfc41c1b6898d290dbe258
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-28 17:16:30 +0200

    feat: wrap ForgetPasswordView in BlocProvider for state management

--- lib/Features/auth/forget_password/presentation/views/forget_password_view.dart
+++ lib/Features/auth/forget_password/presentation/views/forget_password_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart%20';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
+import 'package:stay_match/core/utils/service_locator.dart';
+import 'package:stay_match/features/auth/data/manager/auth_cubit.dart';
+import 'package:stay_match/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:stay_match/features/auth/forget_password/presentation/views/widgets/forget_password_view_body.dart';
 
 class ForgetPasswordView extends StatelessWidget {
@@ -8,7 +12,10 @@ class ForgetPasswordView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(body: const ForgetPasswordViewBody()),
+      child: BlocProvider(
+        create: (context) => AuthCubit(getIt.get<AuthRepoImpl>()),
+        child: Scaffold(body: const ForgetPasswordViewBody()),
+      ),
     );
   }
 }
