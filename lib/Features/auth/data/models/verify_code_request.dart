25b6d58c7997573acaeb09ac172fc1e42dc03ee1
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-28 17:18:02 +0200

    feat: add VerifyCodeRequest and VerifyEmailResponse models for code verification process

---
+++ lib/Features/auth/data/models/verify_code_request.dart
@@ -0,0 +1,23 @@
+/// email : "RahmaIbrahim315@gmail.com"
+/// code : "600"
+
+class VerifyCodeRequest {
+  VerifyCodeRequest({
+      this.email, 
+      this.code,});
+
+  VerifyCodeRequest.fromJson(dynamic json) {
+    email = json['email'];
+    code = json['code'];
+  }
+  String? email;
+  String? code;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['email'] = email;
+    map['code'] = code;
+    return map;
+  }
+
+}
