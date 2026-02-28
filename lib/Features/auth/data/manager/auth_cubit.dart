1bda7586d375bb8e24161aef0de052e922fc2727
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-28 17:11:56 +0200

    feat: implement forget password functionality in AuthCubit with validation

--- lib/Features/auth/data/manager/auth_cubit.dart
+++ lib/Features/auth/data/manager/auth_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
+import 'package:stay_match/features/auth/data/models/forget_password_response.dart';
 import 'package:stay_match/features/auth/data/models/login_response.dart';
 
 import '../repos/auth_repo.dart';
@@ -9,40 +10,87 @@ part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthRepo authRepo;
-  GlobalKey<FormState> loginKey = GlobalKey<FormState>();
+  GlobalKey<FormState> loginKey = GlobalKey<FormState>(debugLabel: 'login form key' );
 
+  //=========== login page ===========
   // email
-  final TextEditingController emailController = TextEditingController();
+  final TextEditingController loginEmailController = TextEditingController(
+    text: 'Rahmaibrahim315@gmail.com',
+  );
 
   // password
-  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController loginPasswordController = TextEditingController(
+    text: 'Meow@123',
+  );
 
+  //=========== forget password page ===========
+  final GlobalKey<FormState> forgetFormKey = GlobalKey<FormState>(debugLabel: 'forget form key' );
+  final TextEditingController forgetEmailController = TextEditingController(
+    text: 'Rahmaibrahim315@gmail.com',
+  );
+
+  //--------methods---------
   AuthCubit(this.authRepo) : super(AuthStateInitial());
 
+  // -------- login ----------
   Future<void> login() async {
     emit(LoginStateLoading());
     var user = await authRepo.login(
-      email: emailController.text,
-      password: passwordController.text,
+      email: loginEmailController.text,
+      password: loginPasswordController.text,
     );
     user.fold(
-            (exception) {
-          emit(LoginStateFailure(errMessage: exception.errMessage));
-        },
-            (response) {
-          if (response.isSuccess == true) {
-            emit(LoginStateSuccess(response));
-          } else {
-            emit(LoginStateFailure(
-                errMessage: response.message ?? "Invalid Email or Password"
-            ));
-          }
+      (exception) {
+        emit(LoginStateFailure(errMessage: exception.errMessage));
+      },
+      (response) {
+        if (response.isSuccess == true) {
+          emit(LoginStateSuccess(response));
+        } else {
+          emit(
+            LoginStateFailure(
+              errMessage: response.message ?? "Invalid Email or Password",
+            ),
+          );
         }
+      },
     );
-  }  String? Function(String?) loginEmailValidator() {
-    return (email) => authRepo.loginEmailValidator(email: email);
   }
-  String? Function(String?) loginPasswordValidator() {
-    return (password) => authRepo.loginPasswordValidator(password: password);
+
+  String? Function(String?) emailValidator() {
+    return (email) => authRepo.emailValidator(email: email);
   }
+
+  String? Function(String?) passwordValidator() {
+    return (password) => authRepo.passwordValidator(password: password);
+  }
+
+  void loginAccountValidation() {
+    final formState = loginKey.currentState;
+    if (formState is FormState && formState.validate() == true) {
+      login();
+    }
+  }
+  // -------- forget password ----------
+  Future<void> sendCode() async{
+    emit(SendCodeStateLoading());
+    var response = await authRepo.sendVerificationCode(email: forgetEmailController.text);
+    response.fold((fail){
+      emit(SendCodeStateFailure(errMessage: fail.errMessage));
+    }, (resp){
+      if(resp.isSuccess == true){
+        SendCodeStateSuccess(resp);
+      }
+      else{
+        SendCodeStateFailure( errMessage: resp.message ?? 'Failed sending code');
+      }
+    });
+  }
+  void emailForForgetPassValidation() {
+    final formState = forgetFormKey.currentState;
+    if (formState is FormState && formState.validate() == true) {
+      sendCode();
+    }
+  }
+// -------- confirm password ----------
 }
