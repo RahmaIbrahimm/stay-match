c243d9e2a8e75fffe843e995298eb758ea3ad096
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-02-28 17:15:07 +0200

    feat: add endpoint for forget password

--- lib/core/networking/endpoints.dart
+++ lib/core/networking/endpoints.dart
@@ -1,4 +1,5 @@
 abstract class Endpoints {
   static String baseUrl = 'https://graduationproject1.runasp.net/';
   static String login = 'login';
+  static String forgetPassword = 'api/Account/forgot-password';
 }
