f11c35ec47ed2524475ec73a703659058febace2
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 20:37:36 +0200

    feat: add Google login clientId to Endpoints class

--- lib/core/networking/endpoints.dart
+++ lib/core/networking/endpoints.dart
@@ -1,7 +1,9 @@
 abstract class Endpoints {
-  static String baseUrl = 'https://graduationproject1.runasp.net/';
-  static String login = 'login';
-  static String forgetPassword = 'api/Account/forgot-password';
-  static String verifyCode = 'api/Account/verify-code';
-  static String resetPassword = 'api/Account/reset-password';
+  static const String baseUrl = 'https://graduationproject1.runasp.net/';
+  static const String login = 'login';
+  static const String forgetPassword = 'api/Account/forgot-password';
+  static const String verifyCode = 'api/Account/verify-code';
+  static const String resetPassword = 'api/Account/reset-password';
+  static const String googleLogin = 'api/Account/google-login';
+  static const String clientId= '753330589269-8ikuvjtbbvhranh8rg52rkodge5mg33h.apps.googleusercontent.com';
 }
