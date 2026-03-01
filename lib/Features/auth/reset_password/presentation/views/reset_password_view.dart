41b0d9935dc9fa2d5678713d9a090b3d5c9d6a93
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 13:04:57 +0200

    feat: update ResetPasswordView to set primary background color

--- lib/Features/auth/reset_password/presentation/views/reset_password_view.dart
+++ lib/Features/auth/reset_password/presentation/views/reset_password_view.dart
@@ -5,11 +5,13 @@ import 'package:stay_match/features/auth/data/manager/auth_cubit.dart';
 import 'package:stay_match/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:stay_match/features/auth/reset_password/presentation/views/widgets/reset_password_view_Body.dart';
 
+import '../../../../../core/constants/app_colors.dart';
+
 class ResetPasswordView extends StatelessWidget {
   const ResetPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(body: ResetPasswordViewBody()));
+    return SafeArea(child: Scaffold(backgroundColor:AppColors.primary,body: ResetPasswordViewBody()));
   }
 }
