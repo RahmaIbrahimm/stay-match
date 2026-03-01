6e192d2daba538448588cd99874165956fc753dd
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 09:14:52 +0200

    feat: update routing for reset password view

--- lib/core/routing/app_routing.dart
+++ lib/core/routing/app_routing.dart
@@ -1,8 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:stay_match/features/auth/login/presentation/views/login_view.dart';
-import 'package:stay_match/features/auth/new_password/presentation/views/reset_password_view.dart';
 import 'package:stay_match/features/auth/signup/presentation/views/signup_view.dart';
 import '../../features/auth/forget_password/presentation/views/forget_password_view.dart';
+import '../../features/auth/reset_password/presentation/views/reset_password_view.dart';
 import '../../features/auth/verify_email/presentation/views/verify_email_view.dart';
 
 class AppRouting {
@@ -10,15 +10,16 @@ class AppRouting {
   static const signupView ='/signupView';
   static const forgetPasswordView ='/forgetPasswordView';
   static const verifyEmailView ='/verifyEmailView';
-  static const newPasswordView ='/newPasswordView';
+  static const resetPasswordView ='/resetPasswordView';
 
   static final router = GoRouter(
+      initialLocation: verifyEmailView,
   routes: [
     GoRoute(path: loginView, builder: (context, state) => const LoginView(),),
     GoRoute(path: signupView,builder: (context, state) => const SignUpView(),),
     GoRoute(path: forgetPasswordView,builder: (context, state) => const ForgetPasswordView(),),
     GoRoute(path: verifyEmailView,builder: (context, state) => const VerifyEmailView(),),
-    GoRoute(path: newPasswordView,builder: (context, state) => const ResetPasswordView(),),
+    GoRoute(path: resetPasswordView,builder: (context, state) => const ResetPasswordView(),),
   ]
   );
 }
