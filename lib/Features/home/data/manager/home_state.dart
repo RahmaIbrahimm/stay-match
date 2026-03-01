2e4a9679ecd094a2594ce175cb5c4acd3d1bc0ad
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-01 13:04:07 +0200

    feat: add HomeState class for managing home view state

---
+++ lib/Features/home/data/manager/home_state.dart
@@ -0,0 +1,10 @@
+part of 'home_cubit.dart';
+
+@immutable
+sealed class HomeState extends Equatable{}
+
+final class HomeInitial extends HomeState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
