9a994b841cf18cbbe844906f93358829edc7675f
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-06 06:30:54 +0200

    feat: add signup method to repo

--- lib/Features/auth/data/repos/auth_repo.dart
+++ lib/Features/auth/data/repos/auth_repo.dart
@@ -1,17 +1,19 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:stay_match/core/errors/failures.dart';
 import 'package:stay_match/features/auth/data/models/forget_password_response.dart';
 import 'package:stay_match/features/auth/data/models/login_with_google_response.dart';
+import 'package:stay_match/features/auth/data/models/register_response.dart';
 import 'package:stay_match/features/auth/data/models/reset_password_response.dart';
 
 import '../models/login_response.dart';
 import '../models/verify_code_response.dart';
 
 abstract class AuthRepo {
-
   String? emailValidator({String? email});
 
   String? passwordValidator({String? password});
+
   // ========== login  =========
   // regular login
 
@@ -19,9 +21,23 @@ abstract class AuthRepo {
     required String email,
     required String password,
   });
+
   // login with google
   Future<Either<Failure, LoginWithGoogleResponse>> loginWithGoogle({
     required String idToken,
+  });
+
+  // ======== signup ==========
+
+  Future<Either<Failure, RegisterResponse>> signup({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String confirmPassword,
+    required String genderType,
+    required String birthDate,
+    required String city,
   });
 
   // ========= forget password =========
