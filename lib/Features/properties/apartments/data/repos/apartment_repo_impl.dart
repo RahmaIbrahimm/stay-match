017922a394f9eec7b25d4b118d439cbb8fbf4b92
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-12 12:44:40 +0200

    feat: add ApartmentsSection widget and ApartmentRepoImpl for apartment data handling

---
+++ lib/Features/properties/apartments/data/repos/apartment_repo_impl.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:stay_match/core/errors/failures.dart';
+import 'package:stay_match/core/networking/api_service.dart';
+
+import '../../../../../core/networking/endpoints.dart';
+import '../models/get_all_apartments.dart';
+import 'apartment_repo.dart';
+
+class ApartmentRepoImpl extends ApartmentRepo {
+  ApiService apiService;
+
+  ApartmentRepoImpl({required this.apiService});
+
+  @override
+  Future<Either<Failure, GetAllApartments>> getAllApartments({
+    String? start,
+    int? monthsCount,
+    String? government,
+    bool? allowsFamilies,
+    bool? allowsChildren,
+    bool? allowsStudents,
+    String? workerGender,
+    double? userLat,
+    double? userLng,
+    bool orderByOldest = false,
+    bool onlyAvailable = false,
+  }) async {
+    try {
+      var response = await apiService.get(
+        Endpoints.getAllApartments,
+        queryParameters: {
+          'start': start,
+          'monthsCount': monthsCount,
+          'government': government,
+          'allowsFamilies': allowsFamilies,
+          'allowsChildren': allowsChildren,
+          'allowsStudents': allowsStudents,
+          'workerGender': workerGender,
+          'userLat': userLat,
+          'userLng': userLng,
+          'orderByOldest': orderByOldest,
+          'onlyAvailable': onlyAvailable,
+        },
+      );
+      return right(GetAllApartments.fromJson(response));
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioError(e));
+    }
+  }
+}
