67917ba5dae7d41e39d889a9468bf781ff025bab
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-12 12:45:23 +0200

    feat: update app routing to include FindRoomView

--- lib/core/routing/app_routing.dart
+++ lib/core/routing/app_routing.dart
@@ -1,10 +1,11 @@
 import 'package:go_router/go_router.dart';
-import 'package:stay_match/features/auth/login/presentation/views/login_view.dart';
-import 'package:stay_match/features/auth/signup/presentation/views/signup_view.dart';
 import 'package:stay_match/features/home/presentation/views/home_view.dart';
-import '../../features/auth/forget_password/presentation/views/forget_password_view.dart';
-import '../../features/auth/reset_password/presentation/views/reset_password_view.dart';
-import '../../features/auth/verify_email/presentation/views/verify_email_view.dart';
+import '../../features/auth/presentation/forget_password/presentation/views/forget_password_view.dart';
+import '../../features/auth/presentation/login/presentation/views/login_view.dart';
+import '../../features/auth/presentation/reset_password/presentation/views/reset_password_view.dart';
+import '../../features/auth/presentation/signup/presentation/views/signup_view.dart';
+import '../../features/auth/presentation/verify_email/presentation/views/verify_email_view.dart';
+import '../../features/properties/rooms/presentation/views/find_room_view.dart';
 
 class AppRouting {
   // auth
@@ -13,12 +14,13 @@ class AppRouting {
   static const forgetPasswordView = '/forgetPasswordView';
   static const verifyEmailView = '/verifyEmailView';
   static const resetPasswordView = '/resetPasswordView';
+  static const findRoomView = '/findRoomView';
 
   // home
   static const homeView = '/homeView';
 
   static final router = GoRouter(
-    initialLocation: homeView,
+    initialLocation: findRoomView,
     routes: [
       GoRoute(path: loginView, builder: (context, state) => const LoginView()),
       GoRoute(
@@ -38,6 +40,7 @@ class AppRouting {
         builder: (context, state) => const ResetPasswordView(),
       ),
       GoRoute(path: homeView, builder: (context, state) => const HomeView()),
+      GoRoute(path: findRoomView, builder: (context, state) => const FindRoomView()),
     ],
   );
 }
