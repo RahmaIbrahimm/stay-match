154725b478b10437ef305a1ee6eaa7029c50d9ee
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-12 12:42:52 +0200

    feat: list of rooms in home page

---
+++ lib/Features/home/presentation/widget/rooms_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../properties/rooms/data/models/get_all_rooms.dart';
+import '../../../properties/rooms/presentation/widgets/room_card.dart';
+
+
+class RoomsList extends StatelessWidget {
+  const RoomsList({
+    super.key,
+    required this.roomPropertiesData,
+    required this.size,
+  });
+
+  final Size size;
+  final List<RoomData>? roomPropertiesData;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: roomPropertiesData?.length ?? 0,
+      itemBuilder: (context, index) {
+        return RoomCard(
+          size: size,
+          coverImageUrl: roomPropertiesData?[index].coverImageUrl,
+          name: roomPropertiesData?[index].name,
+          street: roomPropertiesData?[index].street,
+          city: roomPropertiesData?[index].city,
+          numOfRooms: roomPropertiesData?[index].rooms?.length,
+          monthRent: roomPropertiesData?[index].rooms?[index].monthRent,
+        );
+      },
+    );
+  }
+}
