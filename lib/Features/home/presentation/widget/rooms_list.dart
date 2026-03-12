697bf66837879a0b1eb9ff2292f8ed36973a5f1f
Rahma <rahmaibrahim315@gmail.com>
2026-03-12 17:26:11 +0200

    feat: enhance RoomsList to display message when no rooms are available and add isFurnished property to RoomCard

--- lib/Features/home/presentation/widget/rooms_list.dart
+++ lib/Features/home/presentation/widget/rooms_list.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../properties/rooms/data/models/get_all_rooms.dart';
 import '../../../properties/rooms/presentation/widgets/room_card.dart';
 
-
 class RoomsList extends StatelessWidget {
   const RoomsList({
     super.key,
@@ -16,20 +15,26 @@ class RoomsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: roomPropertiesData?.length ?? 0,
-      itemBuilder: (context, index) {
-        return RoomCard(
-          size: size,
-          coverImageUrl: roomPropertiesData?[index].coverImageUrl,
-          name: roomPropertiesData?[index].name,
-          street: roomPropertiesData?[index].street,
-          city: roomPropertiesData?[index].city,
-          numOfRooms: roomPropertiesData?[index].rooms?.length,
-          monthRent: roomPropertiesData?[index].rooms?[index].monthRent,
-        );
-      },
-    );
+    return roomPropertiesData!.isEmpty
+        ? Center(
+            child: Text('No rooms available', style: TextStyle(fontSize: 30)),
+          )
+        : ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: roomPropertiesData?.length ?? 0,
+            itemBuilder: (context, index) {
+              return RoomCard(
+                size: size,
+                coverImageUrl: roomPropertiesData?[index].coverImageUrl,
+                name: roomPropertiesData?[index].name,
+                street: roomPropertiesData?[index].street,
+                city: roomPropertiesData?[index].city,
+                numOfRooms: roomPropertiesData?[index].rooms?.length,
+                monthRent: roomPropertiesData?[index].rooms?[index].monthRent,
+                // todo: add furnished impl
+                isFurnished: true,
+              );
+            },
+          );
   }
 }
