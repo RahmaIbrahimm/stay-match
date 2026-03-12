3d63274fcd13fda55813c17b8365147c834ec43a
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-12 13:00:16 +0200

    feat: implement MultiBlocProvider in HomeView for state management

--- lib/Features/home/presentation/widget/home_view_body.dart
+++ lib/Features/home/presentation/widget/home_view_body.dart
@@ -1,13 +1,19 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:stay_match/features/properties/manager/properties_cubit.dart';
-import 'package:stay_match/features/properties/presentation/widgets/property_card.dart';
-import 'package:stay_match/features/properties/data/models/get_all_apartments.dart';
+import 'package:stay_match/core/constants/app_colors.dart';
+import 'package:stay_match/core/constants/app_strings.dart';
+import 'package:stay_match/core/constants/app_styles.dart';
+import 'package:stay_match/core/widgets/custom_elevated_button.dart';
+import 'package:stay_match/core/widgets/custom_text_button.dart';
+import 'package:stay_match/features/home/presentation/widget/rooms_list.dart';
+import 'package:stay_match/features/home/presentation/widget/rooms_section.dart';
+import 'package:stay_match/features/properties/rooms/manager/rooms_cubit.dart';
 
-import '../../../../core/networking/dio_consumer.dart';
-import '../../../../core/utils/service_locator.dart';
-import '../../../properties/data/repos/properties_repo_impl.dart';
+import '../../../properties/apartments/data/models/get_all_apartments.dart';
+import '../../../properties/apartments/manager/apartment_cubit.dart';
+import '../../../properties/apartments/presentation/widgets/apartment_list.dart';
+import '../../../properties/rooms/data/models/get_all_rooms.dart';
+import 'apartments_section.dart';
 import 'home_header.dart';
 
 class HomeViewBody extends StatefulWidget {
@@ -20,7 +26,6 @@ class HomeViewBody extends StatefulWidget {
 class _HomeViewBodyState extends State<HomeViewBody>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  List<Data>? properties =[];
 
   @override
   void initState() {
@@ -39,72 +44,95 @@ class _HomeViewBodyState extends State<HomeViewBody>
           SliverList(
             delegate: SliverChildListDelegate([
               HomeHeader(size: size, tabController: _tabController),
-              // todo : all data from network (api)
-                Container(
-                  // height: size.height * 0.2,
-                  // width: size.width * 0.4,
-                  color: Colors.lightBlueAccent[100],
-                  child: BlocConsumer<PropertiesCubit, PropertiesState>(
-                    listener: (context, state) async {
-                      if (state is GetPropertiesSuccess) {
-                        properties = state.response.data;
-                      }
-                      if (state is GetPropertiesFailure) {
-                        // todo: add snack bar / toast
-                      }
-                    },
-                    builder: (context, state) {
-                      // Loading state
-                      // todo: custom loading state?
-                      if (state is GetPropertiesLoading) {
-                        return CircularProgressIndicator();
-                      }
-
-                      // Success state - data is HERE
-                      else if (state is GetPropertiesSuccess) {
-                        if (state.response.data != null && state.response.data!.isNotEmpty) {
-                          return CachedNetworkImage(imageUrl:state.response.data![0].coverImageUrl!);
-                        } else {
-                          // todo: add what would appear when there is no data ?
-                          return Text('No data');
-                        }
-                      }
-
-                      // Failure state
-                      else if (state is GetPropertiesFailure) {
-                        return Text('Error: ${state.errMessage}');
-                      }
-
-                      // Initial state (before any loading)
-                      else {
-                        return Container(); // or loading, or nothing
-                      }
-                    },
-                  ),
+              SizedBox(height: 16),
+              // discover apartments
+              BlocBuilder<PropertiesCubit, ApartmentsState>(
+                builder: (context, state) {
+                  if (state is GetPropertiesSuccess) {
+                    var properties = state.response.data;
+                    if (properties != null && properties.isNotEmpty) {
+                      return ApartmentsSection(
+                        size: size,
+                        properties: properties,
+                      );
+                    }
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                },
+              ),
+              SizedBox(height: 24),
+              // discover rooms
+              BlocBuilder<RoomsCubit, RoomsState>(
+                builder: (context, state) {
+                  if (state is GetRoomsSuccess) {
+                    var roomPropertiesData = state.response.data;
+                    return RoomsSection(
+                      size: size,
+                      roomPropertiesData: roomPropertiesData,
+                    );
+                  }
+                  // Failure state
+                  else if (state is GetRoomsFailure) {
+                    return Text('Error: ${state.errMessage}');
+                  }
+                  // Initial state (before any loading)
+                  else if (state is RoomsInitial) {
+                    return Container(); // or loading, or nothing
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: AppColors.primary,
+                      ),
+                    );
+                  }
+                },
+              ),
+              SizedBox(height: 24),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8,vertical: 8),
+                height: size.height * 0.17,
+                decoration: BoxDecoration(
+                  color: AppColors.bgGrey,
+                  borderRadius: BorderRadius.circular(15),
                 ),
-
-
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(AppStrings.myProperties,style: AppStyles.semiBold15poppins.copyWith(color: AppColors.textColorPrimary),),
+                    const SizedBox(height: 10,),
+                    Text(
+                      AppStrings.addYourProperty,
+                      style: AppStyles.medium15poppins.copyWith(
+                        color: AppColors.textColorPrimary,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 6,),
+                    Text(
+                      AppStrings.shareApartmentDetails,
+                      style: AppStyles.medium10poppins.copyWith(
+                        color: AppColors.textColorSecondary,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 16,),
+                    // todo: implement add property
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 82.0),
+                      child: CustomElevatedButton(
+                        verticalPadding: 6,
+                        textStyle: AppStyles.semiBold15poppins,
+                        text: AppStrings.addProperty,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ]),
           ),
-          SliverFillRemaining(),
         ],
       ),
     );
   }
 }
-// import 'package:cached_network_image/cached_network_image.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:stay_match/core/networking/dio_consumer.dart';
-// import 'package:stay_match/core/utils/service_locator.dart';
-// import 'package:stay_match/features/properties/data/models/get_all_apartments.dart';
-// import 'package:stay_match/features/properties/data/repos/properties_repo_impl.dart';
-// import 'package:stay_match/features/properties/manager/properties_cubit.dart';
-//
-// class PropertyCard extends StatelessWidget {
-//   PropertyCard({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return ;
-//   }
-// }
