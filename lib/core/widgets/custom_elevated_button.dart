859921d56cc563532802781a8da428b5a087fd4c
Rahma Ibrahim <Rahmaibrahim315@gmail.com>
2026-03-12 13:06:38 +0200

    feat: add customizable padding and text style to custom elevated button

--- lib/core/widgets/custom_elevated_button.dart
+++ lib/core/widgets/custom_elevated_button.dart
@@ -9,38 +9,50 @@ class CustomElevatedButton extends StatelessWidget {
     this.isLoading = false,
     required this.text,
     this.textColor,
-    this.icon, required this.onPressed,
+    this.icon,
+    required this.onPressed,
+    this.textStyle,
+    this.verticalPadding,
+    this.horizontalPadding,
   });
 
   final Color? backgroundColor;
   final Color? textColor;
+  final TextStyle? textStyle;
   final String text;
   final bool isLoading;
   final Widget? icon;
   final VoidCallback? onPressed;
+  final double? verticalPadding;
+  final double? horizontalPadding;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         backgroundColor: backgroundColor ?? AppColors.primary,
-        padding: EdgeInsets.symmetric(vertical: 10),
+        padding: EdgeInsets.symmetric(
+          vertical: verticalPadding ?? 10,
+          horizontal: horizontalPadding ?? 0,
+        ),
       ),
 
       onPressed: onPressed,
-      child: isLoading? CircularProgressIndicator(color:  AppColors.textColorWhite,):Row(
-        spacing: 8,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            text,
-            style: AppStyles.cardTitle.copyWith(
-              color: textColor ?? AppColors.containerColor,
+      child: isLoading
+          ? CircularProgressIndicator(color: AppColors.textColorWhite)
+          : Row(
+              spacing: 8,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  text,
+                  style: (textStyle ?? AppStyles.cardTitle).copyWith(
+                    color: textColor ?? AppColors.containerColor,
+                  ),
+                ),
+                if (icon != null) icon!,
+              ],
             ),
-          ),
-          if(icon != null) icon!,
-        ],
-      ),
     );
   }
 }
