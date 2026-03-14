96f84ee38d287ee9e7b23c03186f251848bb5449
Rahma <rahmaibrahim315@gmail.com>
2026-03-15 01:31:27 +0200

    feat: initialize secure storage and load token in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,30 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stay_match/core/theme/app_theme.dart';
 import 'package:stay_match/core/utils/service_locator.dart';
-import 'package:stay_match/features/auth/data/manager/auth_cubit.dart';
 import 'package:stay_match/features/auth/data/repos/auth_repo_impl.dart';
 
 import 'core/routing/app_routing.dart';
-void main() {
+import 'core/utils/secure_storage_helper.dart';
+import 'features/auth/manager/auth_cubit.dart';
+
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
-  runApp(MultiBlocProvider(
-    providers: [
-      BlocProvider(create: (context) => AuthCubit(getIt.get<AuthRepoImpl>())),
-    ],
-    child: StayMatch(),
-  ));
+  await SecureStorageHelper.loadToken();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AuthCubit(getIt.get<AuthRepoImpl>())),
+      ],
+      child: StayMatch(),
+    ),
+  );
 }
 
 class StayMatch extends StatelessWidget {
   const StayMatch({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
