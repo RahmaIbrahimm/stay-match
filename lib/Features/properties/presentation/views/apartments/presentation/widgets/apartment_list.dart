c216a8a18a7daccc407dad01b8f296d9e3db09fd
Rahma <rahmaibrahim315@gmail.com>
2026-03-15 01:21:37 +0200

    feat: add PropertyList widget for displaying a list of apartments

---
+++ lib/Features/properties/presentation/views/apartments/presentation/widgets/apartment_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../data/models/get_all_apartments.dart';
+import 'apartment_card.dart';
+
+class PropertyList extends StatelessWidget {
+  const PropertyList({super.key, required this.properties, required this.size});
+
+  final List<Items>? properties;
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return properties!.isEmpty
+        ? Center(
+            child: Text('No rooms available', style: TextStyle(fontSize: 30)),
+          )
+        : ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: properties!.length,
+            itemBuilder: (context, index) {
+              return ApartmentCard(size: size, property: properties?[index]);
+            },
+          );
+  }
+}
