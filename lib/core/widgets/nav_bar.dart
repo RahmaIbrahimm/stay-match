1cf2e4b7d9be6a9ae4dd5dc51b50a64fd94314d2
Rahma <rahmaibrahim315@gmail.com>
2026-03-15 01:28:10 +0200

    feat: add NavBar widget for navigation between app sections

---
+++ lib/core/widgets/nav_bar.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:stay_match/core/constants/app_colors.dart';
+
+import '../../../core/constants/app_icons.dart';
+import 'custom_bottom_nav_button.dart';
+
+class NavBar extends StatelessWidget {
+  NavBar({super.key, required this.navigationShell});
+
+  final List<Widget> navBarDestinations = [
+    // home
+    NavigationDestination(
+      icon: CustomBottomNavButton(
+        icon: ImageIcon(AssetImage(AppIcons.homeIcon),color: AppColors.primary,),
+      ),
+      selectedIcon: CustomBottomNavButton(
+        selected: true,
+        icon: ImageIcon(
+          AssetImage(AppIcons.homeIcon),
+          color: AppColors.containerColor,
+        ),
+      ),
+      label: '',
+    ),
+    // file exchange
+    NavigationDestination(
+      icon: CustomBottomNavButton(
+        icon: ImageIcon(AssetImage(AppIcons.folderExchangeIcon), size: 24,color: AppColors.primary,),
+      ),
+      selectedIcon: CustomBottomNavButton(
+        selected: true,
+        icon: ImageIcon(
+          AssetImage(AppIcons.folderExchangeIcon),
+          color: AppColors.containerColor,
+          size: 24,
+        ),
+      ),
+      label: '',
+    ),
+    // grid
+    NavigationDestination(
+      icon: CustomBottomNavButton(
+        icon: ImageIcon(AssetImage(AppIcons.gridIcon), size: 24,color: AppColors.primary,),
+      ),
+      selectedIcon: CustomBottomNavButton(
+        selected: true,
+        icon: ImageIcon(
+          AssetImage(AppIcons.gridIcon),
+          color: AppColors.containerColor,
+          size: 24,
+        ),
+      ),
+      label: '',
+    ),
+    // add circle
+    NavigationDestination(
+      icon: CustomBottomNavButton(
+        icon: ImageIcon(AssetImage(AppIcons.addCircleIcon), size: 24,color: AppColors.primary,),
+      ),
+      selectedIcon: CustomBottomNavButton(
+        selected: true,
+        icon: ImageIcon(
+          AssetImage(AppIcons.addCircleIcon),
+          color: AppColors.containerColor,
+          size: 24,
+        ),
+      ),
+      label: '',
+    ),
+
+    // chat bubble
+    NavigationDestination(
+      icon: CustomBottomNavButton(
+        icon: ImageIcon(AssetImage(AppIcons.chatBubbleIcon), size: 24,color: AppColors.primary,),
+      ),
+      selectedIcon: CustomBottomNavButton(
+        selected: true,
+        icon: ImageIcon(
+          AssetImage(AppIcons.chatBubbleIcon),
+          color: AppColors.containerColor,
+          size: 24,
+        ),
+      ),
+      label: '',
+    ),
+    // profile
+    NavigationDestination(
+      icon: CustomBottomNavButton(
+        icon: ImageIcon(AssetImage(AppIcons.profileIcon), size: 24,color: AppColors.primary,),
+      ),
+      selectedIcon: CustomBottomNavButton(
+        selected: true,
+        icon: ImageIcon(
+          AssetImage(AppIcons.profileIcon),
+          color: AppColors.containerColor,
+          size: 24,
+        ),
+      ),
+      label: '',
+    ),
+  ];
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      indicatorColor: Colors.transparent,
+      indicatorShape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      
+      destinations: navBarDestinations,
+      onDestinationSelected: navigationShell.goBranch,
+      backgroundColor: Colors.transparent,
+      selectedIndex: navigationShell.currentIndex,
+    );
+  }
+}
