b9c7d5e4901f41fc352c167ebedff8485e69d345
Rahma <rahmaibrahim315@gmail.com>
2026-03-15 01:28:54 +0200

    feat: implement RoomsRepoImpl for fetching all rooms with various filters

---
+++ lib/Features/properties/presentation/views/rooms/data/repos/rooms_repo_impl.dart
@@ -0,0 +1,55 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:stay_match/core/errors/failures.dart';
+import 'package:stay_match/core/networking/api_service.dart';
+import 'package:stay_match/core/networking/endpoints.dart';
+import 'package:stay_match/features/properties/presentation/views/rooms/data/repos/rooms_repo.dart';
+
+import '../models/get_all_rooms.dart';
+
+class RoomsRepoImpl extends RoomsRepo {
+  ApiService apiService;
+
+  RoomsRepoImpl({required this.apiService});
+
+  @override
+  Future<Either<Failure, GetAllRooms>> getAllRooms({
+    String? start,
+    num? monthsCount,
+    String? government,
+    bool? allowsFamilies,
+    bool? allowsChildren,
+    bool? allowsStudents,
+    String? workerGender,
+    double? userLat,
+    double? userLng,
+    bool orderByOldest = false,
+    bool onlyAvailable = false,
+    num? page,
+    num? pageSize
+  }) async {
+    try {
+      var response = await apiService.get(
+        Endpoints.getAllRooms,
+        queryParameters: {
+          'start': start,
+          'monthsCount': monthsCount,
+          'government': government,
+          'allowsFamilies': allowsFamilies,
+          'allowsChildren': allowsChildren,
+          'allowsStudents': allowsStudents,
+          'workerGender': workerGender,
+          'userLat': userLat,
+          'userLng': userLng,
+          'orderByOldest': orderByOldest,
+          'onlyAvailable': onlyAvailable,
+          'page' : page,
+          'pageSize':pageSize
+        },
+      );
+      return right(GetAllRooms.fromJson(response));
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioError(e));
+    }
+  }
+}
