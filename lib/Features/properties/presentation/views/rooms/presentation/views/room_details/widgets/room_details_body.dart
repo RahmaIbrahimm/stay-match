c355f3d369423a767eff70ed1f3d9558717fdd60
Rahma <rahmaibrahim315@gmail.com>
2026-03-15 01:29:39 +0200

    feat: add RoomDetailsBody widget for displaying room details

---
+++ lib/Features/properties/presentation/views/rooms/presentation/views/room_details/widgets/room_details_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class RoomDetailsBody extends StatelessWidget {
+  const RoomDetailsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
