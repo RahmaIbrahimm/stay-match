0c05828d0df76ac95b7bf2d2fcc3584f671a49fb
Rahma <rahmaibrahim315@gmail.com>
2026-03-15 23:43:59 +0200

    feat: add FindApartmentView widget for apartment search functionality

---
+++ lib/Features/properties/presentation/views/apartments/presentation/views/find_apartment/views/find_apartment_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stay_match/core/networking/dio_consumer.dart';
+import 'package:stay_match/features/properties/presentation/views/apartments/data/repos/apartment_repo_impl.dart';
+import 'package:stay_match/features/properties/presentation/views/apartments/manager/apartment_cubit.dart';
+
+import '../../../../../../../../../core/utils/service_locator.dart';
+import '../widgets/find_apartment_body.dart';
+
+class FindApartmentView extends StatelessWidget {
+  const FindApartmentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: BlocProvider(
+        create: (context) => ApartmentCubit(
+          ApartmentRepoImpl(apiService: getIt.get<DioConsumer>()),
+        )..getAllApartments(),
+        child: Scaffold(body: FindApartmentBody()),
+      ),
+    );
+  }
+}
