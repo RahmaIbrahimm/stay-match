6cbf23cfdeb9cb620c5fe04e044c24bc1c497c90
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:07:34 +0200

    feat: register lazy singletons for apartment, rooms, and home repositories in service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../features/apartments/data/repos/apartment_repo_impl.dart';
 import '../../features/auth/data/repos/auth_repo_impl.dart';
+import '../../features/home/data/repos/home_repo_impl.dart';
+import '../../features/rooms/data/repos/rooms_repo_impl.dart';
 import '../networking/dio_consumer.dart';
 
 final getIt = GetIt.instance;
@@ -9,4 +12,13 @@ final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<DioConsumer>(DioConsumer(Dio()));
   getIt.registerSingleton<AuthRepoImpl>(AuthRepoImpl(getIt.get<DioConsumer>()));
+  getIt.registerLazySingleton<ApartmentRepoImpl>(
+    () => ApartmentRepoImpl(apiService: getIt<DioConsumer>()),
+  );
+  getIt.registerLazySingleton<RoomsRepoImpl>(
+    () => RoomsRepoImpl(apiService: getIt<DioConsumer>()),
+  );
+  getIt.registerLazySingleton<HomeRepoImpl>(
+    () => HomeRepoImpl(apiService: getIt<DioConsumer>()),
+  );
 }
