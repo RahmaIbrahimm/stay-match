f50ca04305fd4afc7b426e34252c4e76706373f6
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:11:31 +0200

    feat: implement ApartmentRepoImpl for apartment data retrieval and management

---
+++ lib/Features/apartments/data/repos/apartment_repo_impl.dart
@@ -0,0 +1,70 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:stay_match/core/errors/failures.dart';
+import 'package:stay_match/core/networking/api_service.dart';
+
+import '../../../../../../../core/networking/endpoints.dart';
+import '../models/all_apartments.dart';
+import '../models/apartment_details_response.dart';
+import 'apartment_repo.dart';
+
+class ApartmentRepoImpl extends ApartmentRepo {
+  ApiService apiService;
+
+  ApartmentRepoImpl({required this.apiService});
+
+  @override
+  Future<Either<Failure, AllApartmentsResponse>> getAllApartments({
+    String? start,
+    int? monthsCount,
+    String? government,
+    bool? allowsFamilies,
+    bool? allowsChildren,
+    bool? allowsStudents,
+    String? workerGender,
+    double? userLat,
+    double? userLng,
+    bool orderByOldest = false,
+    bool onlyAvailable = false,
+    num? page = 1,
+    num? pageSize = 5,
+  }) async {
+    try {
+      var response = await apiService.get(
+        Endpoints.getAllApartments,
+        queryParameters: {
+          'start': start,
+          'monthsCount': monthsCount,
+          'government': government,
+          'allowsFamilies': allowsFamilies,
+          'allowsChildren': allowsChildren,
+          'allowsStudents': allowsStudents,
+          'workerGender': workerGender,
+          'userLat': userLat,
+          'userLng': userLng,
+          'orderByOldest': orderByOldest,
+          'onlyAvailable': onlyAvailable,
+          'page': page,
+          'pageSize': pageSize,
+        },
+      );
+      return right(AllApartmentsResponse.fromJson(response));
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioError(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ApartmentDetailsResponse>> getApartmentDetail({
+    required int id,
+  }) async {
+    try {
+      var response = await apiService.get(
+        '${Endpoints.getApartmentDetails}$id',
+      );
+      return right(ApartmentDetailsResponse.fromJson(response));
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioError(e));
+    }
+  }
+}
