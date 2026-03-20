e5e3d728487ba52341bce36227bc932fdb0f1ecc
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:25:45 +0200

    feat: integrate device preview and screen util for responsive design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stay_match/core/theme/app_theme.dart';
 import 'package:stay_match/core/utils/service_locator.dart';
 import 'package:stay_match/features/auth/data/repos/auth_repo_impl.dart';
@@ -8,10 +10,10 @@ import 'core/routing/app_routing.dart';
 import 'core/utils/secure_storage_helper.dart';
 import 'features/auth/manager/auth_cubit.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
-  await SecureStorageHelper.loadToken();
+  await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.tokenKey);
   runApp(
     MultiBlocProvider(
       providers: [
@@ -27,10 +29,18 @@ class StayMatch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.theme,
-      routerConfig: AppRouting.router,
+    return ScreenUtilInit(
+      designSize: Size(402 , 874),
+      child: DevicePreview(
+        enabled: true,
+        builder: (context) => MaterialApp.router(
+          useInheritedMediaQuery: true,
+          builder: DevicePreview.appBuilder,
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.theme,
+          routerConfig: AppRouting.router,
+        ),
+      ),
     );
   }
 }
