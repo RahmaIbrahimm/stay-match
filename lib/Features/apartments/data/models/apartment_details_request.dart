213887823f8a6075ef7f58f8c9b1f1ae13c1917c
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:10:38 +0200

    feat: add ApartmentDetailsRequest model for handling apartment details API requests

---
+++ lib/Features/apartments/data/models/apartment_details_request.dart
@@ -0,0 +1,23 @@
+/// Roomid : 0
+/// PropertyId : 0
+
+class ApartmentDetailsRequest {
+  ApartmentDetailsRequest({
+      this.roomid, 
+      this.propertyId,});
+
+  ApartmentDetailsRequest.fromJson(dynamic json) {
+    roomid = json['Roomid'];
+    propertyId = json['PropertyId'];
+  }
+  int? roomid;
+  int? propertyId;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['Roomid'] = roomid;
+    map['PropertyId'] = propertyId;
+    return map;
+  }
+
+}
