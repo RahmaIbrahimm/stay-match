0d4c08892841d282d5d614911a3308a9330fcad7
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:07:00 +0200

    feat: enhance CustomElevatedButton with responsive dimensions and additional properties

--- lib/core/widgets/custom_elevated_button.dart
+++ lib/core/widgets/custom_elevated_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 import 'package:stay_match/core/constants/app_styles.dart';
 
@@ -13,7 +14,10 @@ class CustomElevatedButton extends StatelessWidget {
     required this.onPressed,
     this.textStyle,
     this.verticalPadding,
-    this.horizontalPadding, this.borderRadius,
+    this.horizontalPadding,
+    this.borderRadius,
+    this.suffixIcon,
+    this.borderColor, this.maxLines,
   });
 
   final Color? backgroundColor;
@@ -26,34 +30,43 @@ class CustomElevatedButton extends StatelessWidget {
   final double? verticalPadding;
   final double? horizontalPadding;
   final double? borderRadius;
-
+  final Widget? suffixIcon;
+  final Color? borderColor;
+  final int? maxLines;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         backgroundColor: backgroundColor ?? AppColors.primary,
+          side: borderColor != null ? BorderSide(color: borderColor!) : null,
         padding: EdgeInsets.symmetric(
-          vertical: verticalPadding ?? 10,
-          horizontal: horizontalPadding ?? 0,
+          vertical: verticalPadding?.r ?? 10.r,
+          horizontal: horizontalPadding?.r ?? 16.r,
         ),
         shape: borderRadius != null
             ? RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(borderRadius!),
+          borderRadius: BorderRadius.circular(borderRadius!.r),
               )
             : null,
+          minimumSize: (verticalPadding != null || horizontalPadding != null)
+              ? Size(0, 0)
+              : null
       ),
       onPressed: onPressed,
       child: isLoading
           ? CircularProgressIndicator(color: AppColors.textColorWhite)
           : Row(
-              spacing: 8,
+        spacing: 8.w,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                if (suffixIcon != null) suffixIcon!,
                 Text(
                   text,
                   style: (textStyle ?? AppStyles.medium20poppins).copyWith(
                     color: textColor ?? AppColors.containerColor,
+                    overflow: TextOverflow.ellipsis,
                   ),
+                  maxLines: maxLines ?? 1,
                 ),
                 if (icon != null) icon!,
               ],
