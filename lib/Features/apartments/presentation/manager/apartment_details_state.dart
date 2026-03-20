534d788278061497c1e16fddfe143d86466ba706
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:10:43 +0200

    feat: add ApartmentDetailsState for managing apartment details loading and response states

---
+++ lib/Features/apartments/presentation/manager/apartment_details_state.dart
@@ -0,0 +1,34 @@
+part of 'apartment_details_cubit.dart';
+
+sealed class ApartmentDetailsState extends Equatable {
+  const ApartmentDetailsState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class ApartmentDetailsInitial extends ApartmentDetailsState {
+  const ApartmentDetailsInitial();
+}
+
+final class GetApartmentDetailsLoading extends ApartmentDetailsState {
+  const GetApartmentDetailsLoading();
+}
+
+final class GetApartmentDetailsSuccess extends ApartmentDetailsState {
+  final ApartmentDetailsResponse response;
+
+  const GetApartmentDetailsSuccess({required this.response});
+
+  @override
+  List<Object?> get props => [response];
+}
+
+final class GetApartmentDetailsFailure extends ApartmentDetailsState {
+  final String errMessage;
+
+  const GetApartmentDetailsFailure({required this.errMessage});
+
+  @override
+  List<Object?> get props => [errMessage];
+}
