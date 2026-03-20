18c06e903c06f92ae15ccfc934ccc6a6da4e59fe
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:07:28 +0200

    feat: enhance SmallCustomButton with responsive dimensions and customizable text style

--- lib/Features/home/presentation/widget/small_custom_button.dart
+++ lib/Features/home/presentation/widget/small_custom_button.dart
@@ -1,33 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/constants/app_styles.dart';
+
 class SmallCustomButton extends StatelessWidget {
   const SmallCustomButton({
     super.key,
     required this.onPressed,
-    required this.text
+    required this.text,
+    this.textStyle,
   });
 
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
   final String text;
+  final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed:onPressed,
+      onPressed: onPressed,
       style: TextButton.styleFrom(
         backgroundColor: AppColors.primary,
-        padding: EdgeInsets.symmetric(vertical: 5),
+        padding: EdgeInsets.symmetric(vertical: 5.r),
         elevation: 0,
         minimumSize: Size(0, 0),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(15.r),
         ),
       ),
       child: Text(
         text,
-        style: AppStyles.semiBold12Poppins.copyWith(
+        style: (textStyle ?? AppStyles.semiBold12poppins).copyWith(
           color: AppColors.textColorWhite,
         ),
       ),
