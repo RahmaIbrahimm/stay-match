30d1343e62d70501aa455e7dba477bbecfb86c77
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:06:27 +0200

    feat: update CustomBottomNavButton to use responsive padding and border radius

--- lib/core/widgets/custom_bottom_nav_button.dart
+++ lib/core/widgets/custom_bottom_nav_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 
 class CustomBottomNavButton extends StatelessWidget {
@@ -14,10 +15,10 @@ class CustomBottomNavButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(12),
+      padding:  EdgeInsets.all(12.r),
       decoration: BoxDecoration(
-        border: Border.all(color: AppColors.stroke, width: 2),
-        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: AppColors.stroke, width: 2.w),
+        borderRadius: BorderRadius.circular(12.r),
         color: selected ? AppColors.primary : Colors.white,
       ),
       child: icon,
