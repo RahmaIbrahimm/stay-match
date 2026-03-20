77c831bfc12f3dff1da5f9463986b6ac5cf523b3
Rahma <rahmaibrahim315@gmail.com>
2026-03-20 06:07:17 +0200

    feat: refactor RoomsList to use responsive dimensions and improve layout

--- lib/Features/home/presentation/widget/rooms_list.dart
+++ lib/Features/home/presentation/widget/rooms_list.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../properties/presentation/views/rooms/data/models/get_all_rooms.dart';
-import '../../../properties/presentation/views/rooms/presentation/widgets/room_card.dart';
+import '../../../rooms/data/models/get_all_rooms.dart';
+import '../../../rooms/presentation/widgets/shared/room_card.dart';
 
 class RoomsList extends StatelessWidget {
-  const RoomsList({
-    super.key,
-    required this.roomPropertiesData,
-    required this.size,
-  });
+  const RoomsList({super.key, required this.roomPropertiesData});
 
-  final Size size;
-  final List<RoomData>? roomPropertiesData;
+  final List<Items>? roomPropertiesData;
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return roomPropertiesData!.isEmpty
         ? Center(
-            child: Text('No rooms available', style: TextStyle(fontSize: 30)),
+            child: Text(
+              'No rooms available',
+              style: TextStyle(fontSize: 30.sp),
+            ),
           )
-        : ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: roomPropertiesData?.length ?? 0,
-            itemBuilder: (context, index) {
-              return RoomCard(
-                size: size,
-                coverImageUrl: roomPropertiesData?[index].coverImageUrl,
-                name: roomPropertiesData?[index].name,
-                street: roomPropertiesData?[index].street,
-                city: roomPropertiesData?[index].city,
+        : SizedBox(
+            height: 275.h,
+            child: CustomScrollView(
+              scrollDirection: Axis.horizontal,
+              slivers: [
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return RoomCard(
+                        coverImageUrl: roomPropertiesData?[index].coverImageUrl,
+                        name: roomPropertiesData?[index].name,
+                        street: roomPropertiesData?[index].street,
+                        city: roomPropertiesData?[index].city,
 
-                // todo: add furnished impl
-                isFurnished: true,
-                id: roomPropertiesData![index].id!,
-                rooms: roomPropertiesData?[index].rooms ?? [],
-              );
-            },
+                        // todo: add furnished impl
+                        id: roomPropertiesData![index].id!.toInt(),
+                        rooms: roomPropertiesData?[index].rooms ?? [],
+                      );
+                    },
+                    childCount: (roomPropertiesData?.length ?? 0) > 3
+                        ? 3
+                        : roomPropertiesData?.length,
+                  ),
+                ),
+              ],
+            ),
           );
   }
 }
