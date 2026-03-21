99fe0571a248839c74cd3fbedc3a4cfffbeba5a8
Rahma <rahmaibrahim315@gmail.com>
2026-03-22 01:31:28 +0200

    feat: add AvailabilitySliver widget for displaying availability options

---
+++ lib/Features/filter/presentation/widgets/availability_sliver.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/constants/app_colors.dart';
+import '../../../../core/constants/app_styles.dart';
+import '../../../../core/widgets/custom_toggle_switch.dart';
+
+enum FilterType { apartment, rooms }
+
+class AvailabilitySliver extends StatelessWidget {
+  const AvailabilitySliver({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: RPadding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Availability',
+                    style: AppStyles.semiBold18poppins.copyWith(
+                      color: AppColors.textColorPrimary,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '\nShow only available units',
+                    style: AppStyles.regular14poppins.copyWith(
+                      color: AppColors.textColorSecondary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // todo: implement filtering for availability
+            CustomToggleSwitch(onTap: () {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
