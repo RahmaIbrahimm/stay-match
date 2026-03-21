e66a260d0af6cc5be4f2567cc11306ede8fca3c7
Rahma <rahmaibrahim315@gmail.com>
2026-03-22 01:34:32 +0200

    feat: add retry mechanism in RoomsSection for handling room loading failures

--- lib/Features/home/presentation/widget/rooms_section.dart
+++ lib/Features/home/presentation/widget/rooms_section.dart
@@ -1,10 +1,14 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 
 import 'package:stay_match/features/home/presentation/widget/rooms_list.dart';
 import 'package:stay_match/features/home/presentation/widget/rooms_section_container.dart';
 
+import '../../../../core/constants/app_styles.dart';
 import '../../../rooms/presentation/manager/rooms_cubit.dart';
 
 class RoomsSection extends StatelessWidget {
@@ -17,6 +21,7 @@ class RoomsSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<RoomsCubit, RoomsState>(
       builder: (context, state) {
+        log('Current state: $state');
         if (state is GetRoomsSuccess) {
           var roomPropertiesData = state.response.data?.items;
           return RoomsSectionContainer(
@@ -27,9 +32,36 @@ class RoomsSection extends StatelessWidget {
         }
         // Failure state
         else if (state is GetRoomsFailure) {
-          return Text('Error: ${state.errMessage}');
+          return RoomsSectionContainer(
+            widget: Center(
+              child: GestureDetector(
+                onTap: () {
+                  context.read<RoomsCubit>().getAllRooms();
+                },
+                child: RPadding(
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.refresh,
+                        size: 18.sp,
+                        color: AppColors.textColorSecondary, // 👈 not red
+                      ),
+                      SizedBox(width: 6.w),
+                      Text(
+                        'Tap to retry',
+                        style: AppStyles.medium14poppins.copyWith(
+                          color: AppColors.textColorSecondary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
         }
-        // Initial state (before any loading)
         else if (state is RoomsInitial) {
           return Container(); // todo: or loading, or nothing
         } else if (state is GetRoomsLoading) {
@@ -40,13 +72,8 @@ class RoomsSection extends StatelessWidget {
               ),
             ),
           );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(
-              color: AppColors.primary,
-            ),
-          );
         }
+        return SizedBox.shrink();
       },
     );
   }
