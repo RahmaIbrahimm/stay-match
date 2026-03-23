3b4392a783eed8d49ce82fc9631c309e75439e86
Rahma <rahmaibrahim315@gmail.com>
2026-03-23 05:37:13 +0200

    refactor: convert FilterOptionCard to StatefulWidget and enhance interactivity

--- lib/Features/filter/presentation/widgets/filter_option_card.dart
+++ lib/Features/filter/presentation/widgets/filter_option_card.dart
@@ -1,73 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stay_match/features/filter/presentation/widgets/filter_helper.dart';
 
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/constants/app_styles.dart';
 
-class FilterOptionCard extends StatelessWidget {
-  const FilterOptionCard({
+class FilterOptionCard extends StatefulWidget {
+   FilterOptionCard({
     super.key,
     required this.title,
     this.desc,
     required this.icon,
     this.bottomWidget,
-    this.isSelected = false,
+    required this.isSelected,
+    required this.onChangeVoid,
+     required this.onChangedBool,
   });
 
   final String title;
   final String? desc;
   final Widget icon;
   final Widget? bottomWidget;
-  final bool isSelected;
+   bool isSelected;
+  final  OnChangedBool onChangedBool;
+  final VoidCallback onChangeVoid;
+  @override
+  State<FilterOptionCard> createState() => _FilterOptionCardState();
+}
 
+class _FilterOptionCardState extends State<FilterOptionCard> {
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16.r, vertical: 16.r),
-        margin: EdgeInsets.only(left: 8.r, right: 8.r),
-        decoration: BoxDecoration(
-          color: AppColors.containerColor,
-          border:  Border.all(color: isSelected ? AppColors.primary: AppColors.blueGrey) ,
-          borderRadius: BorderRadius.circular(10.r),
-        ),
-        child: Column(
-          spacing: 16.h,
-          children: [
-            Row(
-              spacing: 16.w,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(16.r),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.r),
-                    color: AppColors.blueGrey,
+      child: GestureDetector(
+        onTap: () {
+            widget.onChangeVoid();
+
+        },
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 0),
+          padding: EdgeInsets.symmetric(horizontal: 16.r, vertical: 16.r),
+          margin: EdgeInsets.only(left: 8.r, right: 8.r),
+          decoration: BoxDecoration(
+            color: AppColors.containerColor,
+            border:  Border.all(color: widget.isSelected ? AppColors.primary: AppColors.blueGrey) ,
+            borderRadius: BorderRadius.circular(10.r),
+          ),
+          child: Column(
+            spacing: 16.h,
+            children: [
+              Row(
+                spacing: 16.w,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(16.r),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.r),
+                      color: AppColors.blueGrey,
+                    ),
+                    child: widget.icon,
                   ),
-                  child: icon,
-                ),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: title,
-                        style: AppStyles.semiBold14poppins.copyWith(
-                          color: AppColors.textColorPrimary,
-                        ),
-                      ),
-                      if (desc != null)
+                  RichText(
+                    text: TextSpan(
+                      children: [
                         TextSpan(
-                          text: desc,
-                          style: AppStyles.regular12poppins.copyWith(
-                            color: AppColors.textColorSecondary,
+                          text: widget.title,
+                          style: AppStyles.semiBold14poppins.copyWith(
+                            color: AppColors.textColorPrimary,
                           ),
                         ),
-                    ],
+                        if (widget.desc != null)
+                          TextSpan(
+                            text: widget.desc,
+                            style: AppStyles.regular12poppins.copyWith(
+                              color: AppColors.textColorSecondary,
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            if (bottomWidget != null) bottomWidget!,
-          ],
+                ],
+              ),
+              if (widget.bottomWidget != null) widget.bottomWidget!,
+            ],
+          ),
         ),
       ),
     );
