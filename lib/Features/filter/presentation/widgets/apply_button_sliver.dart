0c308fc138c658e589d49f891de22758c29beacb
Rahma <rahmaibrahim315@gmail.com>
2026-03-23 03:31:24 +0200

    feat: add isLoading parameter to ApplyButtonSliver for loading state management

--- lib/Features/filter/presentation/widgets/apply_button_sliver.dart
+++ lib/Features/filter/presentation/widgets/apply_button_sliver.dart
@@ -5,13 +5,18 @@ import '../../../../core/constants/app_strings.dart';
 import '../../../../core/constants/app_styles.dart';
 import '../../../../core/widgets/custom_elevated_button.dart';
 class ApplyButtonSliver extends StatelessWidget {
-  const ApplyButtonSliver({super.key, required this.onPressed});
+   ApplyButtonSliver({super.key, required this.onPressed, this.isLoading});
 final VoidCallback onPressed;
+  bool? isLoading;
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(child: RPadding(
       padding: const EdgeInsets.all(16.0),
-      child: CustomElevatedButton(text: AppStrings.apply, onPressed: (){},textStyle: AppStyles.bold18poppins,borderRadius: 12,),
+      child: CustomElevatedButton(text: AppStrings.apply,
+        onPressed: onPressed,
+        textStyle: AppStyles.bold18poppins,
+        borderRadius: 12,
+        isLoading: isLoading ?? false,),
     ))
     ;
   }
