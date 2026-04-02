db9ecf735f702d51dc1788962de317cc5687fca0
Rahma <rahmaibrahim315@gmail.com>
2026-04-02 20:37:43 +0200

    refactor: simplify RoomsSection widget and improve failure handling

--- lib/Features/home/presentation/widget/rooms_section.dart
+++ lib/Features/home/presentation/widget/rooms_section.dart
@@ -2,20 +2,15 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
+import 'package:stay_match/Features/home/presentation/widget/rooms_list.dart';
+import 'package:stay_match/Features/home/presentation/widget/rooms_section_container.dart';
+import 'package:stay_match/Features/home/presentation/widget/section_failure.dart';
 
-import 'package:stay_match/features/home/presentation/widget/rooms_list.dart';
-import 'package:stay_match/features/home/presentation/widget/rooms_section_container.dart';
-
-import '../../../../core/constants/app_styles.dart';
 import '../../../rooms/presentation/manager/rooms_cubit.dart';
 
 class RoomsSection extends StatelessWidget {
-  const RoomsSection({
-    super.key,
-  });
-
+  const RoomsSection({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,51 +20,18 @@ class RoomsSection extends StatelessWidget {
         if (state is GetRoomsSuccess) {
           var roomPropertiesData = state.response.data?.items;
           return RoomsSectionContainer(
-            widget: RoomsList(
-              roomPropertiesData: roomPropertiesData,
-            ),
+            widget: RoomsList(roomPropertiesData: roomPropertiesData),
           );
         }
         // Failure state
         else if (state is GetRoomsFailure) {
-          return RoomsSectionContainer(
-            widget: Center(
-              child: GestureDetector(
-                onTap: () {
-                  context.read<RoomsCubit>().getAllRooms();
-                },
-                child: RPadding(
-                  padding: EdgeInsets.all(16),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        Icons.refresh,
-                        size: 18.sp,
-                        color: AppColors.textColorSecondary, // 👈 not red
-                      ),
-                      SizedBox(width: 6.w),
-                      Text(
-                        'Tap to retry',
-                        style: AppStyles.medium14poppins.copyWith(
-                          color: AppColors.textColorSecondary,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-        else if (state is RoomsInitial) {
+          return RoomsSectionContainer(widget: SectionFailure(property: Property.room,));
+        } else if (state is RoomsInitial) {
           return Container(); // todo: or loading, or nothing
         } else if (state is GetRoomsLoading) {
           return RoomsSectionContainer(
             widget: Center(
-              child: CircularProgressIndicator(
-                color: AppColors.primary,
-              ),
+              child: CircularProgressIndicator(color: AppColors.primary),
             ),
           );
         }
