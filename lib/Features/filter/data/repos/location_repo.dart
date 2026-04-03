471e734973cd8178707195697f616e1800038a4c
Rahma <rahmaibrahim315@gmail.com>
2026-04-04 00:46:42 +0200

    feat: add LocationRepo abstract class with getGovernorates method

---
+++ lib/Features/filter/data/repos/location_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../models/location_model.dart';
+
+abstract class LocationRepo {
+  Future<Either<Failure, List<Governorate>>>  getGovernorates();
+}
