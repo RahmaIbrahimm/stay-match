3cd39280e43d3c4a52dea8c2207affc81cc1bbfa
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:33:19 +0200

    style: format colors and add fieldFillColor to AppColors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -14,12 +14,13 @@ abstract class AppColors {
   static const textColorSuccess = Color(0xff0D9467);
   static const shadowColor = Color(0x33000000);
   static const blue = Color(0xffA4D5EA);
-  static const grey = Color.fromARGB(200,243, 244, 246);
-  static const pink= Color.fromRGBO(255, 186, 225, 0.7);
+  static const grey = Color.fromARGB(200, 243, 244, 246);
+  static const pink = Color.fromRGBO(255, 186, 225, 0.7);
   static const bgGrey = Color(0xffEEEEEF);
   static const darkerGrey = Color(0xffF6F7F8);
   static const blueGrey = Color.fromRGBO(30, 58, 138, 0.1);
   static const stroke = Color(0xffE5E7EB);
+  static const fieldFillColor = Color(0xFFF7F9FC);
 
   static const boxShadow = [
     BoxShadow(
