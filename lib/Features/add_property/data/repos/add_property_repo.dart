79f12f7c26803b980577d0fb9d0bcb96f94a8326
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 17:42:38 +0200

    feat: add AddPropertyRepo abstract class with apartment, room, and image upload methods

---
+++ lib/Features/add_property/data/repos/add_property_repo.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:stay_match/Features/add_property/data/models/upload_image_response.dart';
+import 'package:stay_match/core/errors/failures.dart';
+
+import '../models/add_apartment_request.dart';
+import '../models/add_apartment_response.dart';
+import '../models/add_room_request.dart';
+import '../models/add_room_response.dart';
+
+abstract class AddPropertyRepo {
+
+
+  Future<Either<Failure, AddApartmentResponse>> addApartment({
+    required AddApartmentRequest request,
+  });
+  Future<Either<Failure, AddRoomResponse>> addRoom({
+    required AddRoomRequest request,
+  });
+  Future<Either<Failure, UploadImageResponse>> uploadImg({
+    required String file,
+    required bool isCover
+  });
+}
