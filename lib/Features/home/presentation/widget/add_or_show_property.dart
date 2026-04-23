cc87f013f845918ede09398ce6ee79d040763c81
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 17:38:35 +0200

    feat: implement BlocBuilder to display or add properties in AddOrShowMyProperties widget

--- lib/Features/home/presentation/widget/add_or_show_property.dart
+++ lib/Features/home/presentation/widget/add_or_show_property.dart
@@ -1,60 +1,207 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:stay_match/Features/home/presentation/widget/section_failure.dart';
+import 'package:stay_match/Features/my_properties/presentation/widgets/my_property_card.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 import 'package:stay_match/core/constants/app_strings.dart';
 import 'package:stay_match/core/constants/app_styles.dart';
 import 'package:stay_match/core/widgets/custom_elevated_button.dart';
-class AddOrShowMyProperties extends StatelessWidget {
-  const AddOrShowMyProperties({
-    super.key,
-  });
 
+import '../../../../core/routing/app_routing.dart';
+import '../../../../core/widgets/custom_text_button.dart';
+import '../../../my_properties/presentation/manager/my_properties_cubit.dart';
+
+// class AddOrShowMyProperties extends StatelessWidget {
+//   const AddOrShowMyProperties({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       padding: EdgeInsets.symmetric(horizontal: 10.r, vertical: 12.r),
+//       // Removed fixed height: the container now shrinks to fit its children
+//       decoration: BoxDecoration(
+//         color: AppColors.bgGrey,
+//         borderRadius: BorderRadius.circular(16.r),
+//       ),
+//       child: Column(
+//         mainAxisSize: MainAxisSize.min,
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           Text(
+//             AppStrings.myProperties,
+//             style: AppStyles.semiBold18poppins.copyWith(
+//               color: AppColors.textColorPrimary,
+//             ),
+//           ),
+//           SizedBox(height: 10.h),
+//
+//           BlocBuilder<MyPropertiesCubit, MyPropertiesState>(
+//             builder: (context, state) {
+//               if (state is MyPropertiesLoading) {
+//                 return RPadding(
+//                   padding: EdgeInsets.all(20.r),
+//                   child: const Center(child: CircularProgressIndicator(color: AppColors.primary)),
+//                 );
+//               }
+//
+//               if (state is MyPropertiesSuccess) {
+//                 final properties = state.response.data?.properties;
+//
+//                 if (properties == null || properties.isEmpty) {
+//                   return Column(
+//                     children: [
+//                       Text(
+//                         AppStrings.addYourProperty,
+//                         style: AppStyles.medium16poppins.copyWith(color: AppColors.textColorPrimary),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                       SizedBox(height: 4.h),
+//                       Text(
+//                         AppStrings.shareApartmentDetails,
+//                         style: AppStyles.medium12poppins.copyWith(color: AppColors.textColorSecondary),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                       SizedBox(height: 16.h),
+//                       Padding(
+//                         padding: EdgeInsets.symmetric(horizontal: 50.r),
+//                         child: CustomElevatedButton(
+//                           verticalPadding: 8.r,
+//                           text: AppStrings.addProperty,
+//                           onPressed: () {
+//                             StatefulNavigationShell.of(context).goBranch(1);
+//                             context.goNamed(AppRouting.addPropertyName);
+//                           },
+//                         ),
+//                       ),
+//                     ],
+//                   );
+//                 }
+//
+//                 // If showing cards, we need a height for the horizontal list
+//                 return SizedBox(
+//                   height: 220.h, // We still need a height for Horizontal lists
+//                   child: ListView.separated(
+//                     scrollDirection: Axis.horizontal,
+//                     itemCount: properties.length,
+//                     separatorBuilder: (context, index) => SizedBox(width: 10.w),
+//                     itemBuilder: (context, index) => MyPropertyCard(property: properties[index]),
+//                   ),
+//                 );
+//               }
+//
+//               if (state is MyPropertiesFailure) {
+//                 return SectionFailure(property: Property.myProperties);
+//               }
+//
+//               return const SizedBox.shrink();
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+class AddOrShowMyProperties extends StatelessWidget {
+  const AddOrShowMyProperties({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10.r, vertical: 8.r),
-      height: 190.h,
+      padding: EdgeInsets.symmetric(horizontal: 10.r, vertical: 12.r),
       decoration: BoxDecoration(
         color: AppColors.bgGrey,
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(16.r),
       ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-           SizedBox(height: 4.h),
-          Text(
-            AppStrings.myProperties,
-            style: AppStyles.semiBold18poppins.copyWith(
-              color: AppColors.textColorPrimary,
-            ),
+          // Updated Header with Row to include "View All"
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppStrings.myProperties,
+                style: AppStyles.semiBold18poppins.copyWith(
+                  color: AppColors.textColorPrimary,
+                ),
+              ),
+              // Your requested CustomTextButton implementation
+              CustomTextButton(
+                onPressed: () {
+                  // Ensure this route is defined for showing all your properties
+                  // context.pushNamed(AppRouting.m);
+                },
+                text: AppStrings.viewAllProperties, // Typically just "View All" for sections
+                textColor: AppColors.primary,
+                textStyle: AppStyles.semiBold12poppins,
+                isPadded: false,
+              ),
+            ],
           ),
-           SizedBox(height: 12.h),
-          Text(
-            AppStrings.addYourProperty,
-            style: AppStyles.medium18poppins.copyWith(
-              color: AppColors.textColorPrimary,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: 6.h),
-          Text(
-            AppStrings.shareApartmentDetails,
-            style: AppStyles.medium12poppins.copyWith(
-              color: AppColors.textColorSecondary,
-            ),
-            textAlign: TextAlign.center,
-          ),
-           SizedBox(height: 16.h),
-          // todo: implement add property
-          Padding(
-            padding:  EdgeInsets.symmetric(horizontal: 80.0.r),
-            child: CustomElevatedButton(
-              verticalPadding: 6.r,
-              textStyle: AppStyles.semiBold14poppins,
-              text: AppStrings.addProperty,
-              onPressed: () {},
-            ),
+          SizedBox(height: 10.h),
+
+          BlocBuilder<MyPropertiesCubit, MyPropertiesState>(
+            builder: (context, state) {
+              if (state is MyPropertiesLoading) {
+                return RPadding(
+                  padding: EdgeInsets.all(20.r),
+                  child: const Center(child: CircularProgressIndicator(color: AppColors.primary)),
+                );
+              }
+
+              if (state is MyPropertiesSuccess) {
+                final properties = state.response.data?.properties;
+
+                if (properties == null || properties.isEmpty) {
+                  return Column(
+                    children: [
+                      Text(
+                        AppStrings.addYourProperty,
+                        style: AppStyles.medium16poppins.copyWith(color: AppColors.textColorPrimary),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 4.h),
+                      Text(
+                        AppStrings.shareApartmentDetails,
+                        style: AppStyles.medium12poppins.copyWith(color: AppColors.textColorSecondary),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 16.h),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 50.r),
+                        child: CustomElevatedButton(
+                          verticalPadding: 8.r,
+                          text: AppStrings.addProperty,
+                          onPressed: () {
+                            StatefulNavigationShell.of(context).goBranch(1);
+                            context.goNamed(AppRouting.addPropertyName);
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                }
+
+                return SizedBox(
+                  height: 230.h,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: properties.length,
+                    separatorBuilder: (context, index) => SizedBox(width: 10.w),
+                    itemBuilder: (context, index) => MyPropertyCard(property: properties[index]),
+                  ),
+                );
+              }
+
+              if (state is MyPropertiesFailure) {
+                return SectionFailure(property: Property.myProperties);
+              }
+
+              return const SizedBox.shrink();
+            },
           ),
         ],
       ),
