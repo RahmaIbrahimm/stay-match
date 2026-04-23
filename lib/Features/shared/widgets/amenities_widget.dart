99ecf5cb068757cdaccadcdc99428e71e9f932e6
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:33:38 +0200

    refactor: move AmenitiesWidget to shared features and update imports

--- lib/Features/shared/widgets/amenities_widget.dart
+++ lib/Features/shared/widgets/amenities_widget.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../constants/app_colors.dart';
-import '../constants/app_styles.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/app_styles.dart';
+
 class AmenitiesWidget extends StatelessWidget {
-  const AmenitiesWidget({
-    super.key,
-    required this.amenities,
-  });
+  const AmenitiesWidget({super.key, required this.amenities});
 
   final List<Map<String, dynamic>> amenities;
 
@@ -17,20 +15,17 @@ class AmenitiesWidget extends StatelessWidget {
       child: RPadding(
         padding: const EdgeInsets.all(16),
         child: Wrap(
-          spacing: 20.w, // horizontal spacing
-          runSpacing: 12.h, // vertical spacing
+          spacing: 20.w,
+          runSpacing: 12.h,
           alignment: WrapAlignment.start,
           children: amenities.map((item) {
+
             return SizedBox(
               width: 150.w,
               child: Row(
                 spacing: 12.w,
                 children: [
-                  Icon(
-                    item['icon'],
-                    color: AppColors.primary,
-                    size: 20.sp,
-                  ),
+                  Icon(item['icon'], color: AppColors.primary, size: 20.sp),
                   Text(
                     item['name'],
                     style: AppStyles.regular14poppins.copyWith(
