3de0d9fa7feab875555f9f1bcb1a96083614d874
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:34:14 +0200

    feat: add MyPropertiesCubit to HomeView providers and format BlocProvider initializations

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:stay_match/core/networking/dio_consumer.dart';
+import 'package:stay_match/Features/my_properties/presentation/manager/my_properties_cubit.dart';
 import 'package:stay_match/core/utils/service_locator.dart';
 
 import '../../../apartments/data/repos/apartment_repo_impl.dart';
 import '../../../apartments/presentation/manager/apartment_cubit.dart';
+import '../../../my_properties/data/repos/my_properties_repo.dart';
 import '../../../rooms/data/repos/rooms_repo_impl.dart';
 import '../../../rooms/presentation/manager/rooms_cubit.dart';
 import '../../data/repos/home_repo_impl.dart';
@@ -20,25 +21,22 @@ class HomeView extends StatelessWidget {
       child: MultiBlocProvider(
         providers: [
           BlocProvider(
-            create: (context) => ApartmentCubit(
-              getIt.get<ApartmentRepoImpl>(),
+            create: (context) => ApartmentCubit(getIt.get<ApartmentRepoImpl>()),
+          ),
+          BlocProvider(
+            create: (context) => RoomsCubit(getIt.get<RoomsRepoImpl>()),
+          ),
+          BlocProvider(
+            create: (context) => HomeCubit(getIt.get<HomeRepoImpl>()),
+          ),
+          BlocProvider(
+            create: (context) => MyPropertiesCubit(
+              myPropertiesRepo: getIt.get<MyPropertiesRepo>(),
             ),
-          ),
-          BlocProvider(
-            create: (context) =>
-                RoomsCubit(getIt.get<RoomsRepoImpl>()),
-          ),
-          BlocProvider(
-            create: (context) =>
-                HomeCubit(getIt.get<HomeRepoImpl>()),
           ),
         ],
         // add home cubit
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: HomeViewBody(),
-          // bottomNavigationBar: HomeNavBar(navigationShell: navigationShell),
-        ),
+        child: Scaffold(backgroundColor: Colors.white, body: HomeViewBody()),
       ),
     );
   }
