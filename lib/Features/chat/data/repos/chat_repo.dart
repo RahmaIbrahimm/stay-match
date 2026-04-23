ab9a2ba6f07ccb98366aca6a7b0bde710a5a8082
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:41:54 +0200

    feat: add ChatRepo abstract class with chat management methods

---
+++ lib/Features/chat/data/repos/chat_repo.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:stay_match/Features/chat/data/models/mark_read.dart';
+import 'package:stay_match/Features/chat/data/models/my_chats.dart';
+import 'package:stay_match/Features/chat/data/models/start_chat_response.dart';
+import 'package:stay_match/core/errors/failures.dart';
+
+import '../models/send_message_response.dart';
+
+abstract class ChatRepo {
+  Future<Either<Failure, MyChats>> getMyChats();
+
+  Future<Either<Failure, StartChatResponse>> startChat({
+    required String otherUserId,
+  });
+
+  Future<Either<Failure, MarkRead>> markAsRead({required int chatId});
+
+  Future<Either<Failure, SendMessageResponse>> sendMessage({
+    required int chatId,
+    String? content,
+    String? filePath,
+    required String type, // هنا هنبعتها String زي ما هو واضح في الصورة (مثلاً "Text")
+  });
+}
