01de923c9f32c352e7e0c7e9ebf4f6cab32e4122
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 17:41:41 +0200

    feat: add AddPropertyButtons widget for property creation flow

---
+++ lib/Features/add_property/presentation/widgets/shared/add_property_buttons.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../../core/constants/app_colors.dart';
+import '../../../../../core/constants/app_strings.dart';
+import '../../../../../core/widgets/custom_elevated_button.dart';
+import '../../manager/add_property_cubit.dart';
+
+class AddPropertyButtons extends StatelessWidget {
+  const AddPropertyButtons({
+    super.key,
+    this.hasBackButton = true,
+    this.submit = false,
+    required this.nextPageRoute,
+    required this.cubit,
+    this.onNextPressed,
+  });
+
+  final bool hasBackButton;
+  final String nextPageRoute;
+  final AddPropertyCubit cubit;
+  final bool submit;
+  final VoidCallback? onNextPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // back button
+        if (hasBackButton)
+          Expanded(
+            flex: 3,
+            child: CustomElevatedButton(
+              text: AppStrings.back,
+              onPressed: () {
+                context.read<AddPropertyCubit>().prevStep();
+                Future.microtask(() {
+                  if (context.mounted) {
+                    context.pop();
+                  }
+                });
+              },
+              borderRadius: 12.r,
+              backgroundColor: AppColors.containerColor,
+              textColor: AppColors.primary,
+              borderColor: AppColors.primary,
+            ),
+          ),
+        if (hasBackButton) SizedBox(width: 12.w),
+        // next button
+        Expanded(
+          flex: 5,
+          child: CustomElevatedButton(
+            text: AppStrings.next,
+            onPressed: () {
+              if (onNextPressed != null) {
+                onNextPressed!();
+              } else {
+                if (submit) {
+                  cubit.submitProperty();
+                } else {
+                  cubit.nextStep();
+                  context.pushNamed(nextPageRoute);
+                }
+              }
+            },
+            borderRadius: 12.r,
+          ),
+        ),
+      ],
+    );
+  }
+}
