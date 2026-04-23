e3ae491f050f3b2f4b801c2f9c2811355916a6e5
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 17:37:53 +0200

    style: fix formatting and spacing in ApplyButton widget

--- lib/Features/filter/presentation/widgets/apply_button_sliver.dart
+++ lib/Features/filter/presentation/widgets/apply_button_sliver.dart
@@ -4,20 +4,22 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/constants/app_strings.dart';
 import '../../../../core/constants/app_styles.dart';
 import '../../../../core/widgets/custom_elevated_button.dart';
+
 class ApplyButton extends StatelessWidget {
-   ApplyButton({super.key, required this.onPressed, this.isLoading});
-final VoidCallback onPressed;
+  ApplyButton({super.key, required this.onPressed, this.isLoading});
+  final VoidCallback onPressed;
   bool? isLoading;
   @override
   Widget build(BuildContext context) {
     return RPadding(
       padding: const EdgeInsets.all(16.0),
-      child: CustomElevatedButton(text: AppStrings.apply,
+      child: CustomElevatedButton(
+        text: AppStrings.apply,
         onPressed: onPressed,
         textStyle: AppStyles.bold18poppins,
         borderRadius: 12,
-        isLoading: isLoading ?? false,),
-    )
-    ;
+        isLoading: isLoading ?? false,
+      ),
+    );
   }
 }
