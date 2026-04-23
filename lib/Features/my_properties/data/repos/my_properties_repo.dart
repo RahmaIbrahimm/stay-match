801e545fcf0773b0d6917d104f2078b573a72e89
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:36:15 +0200

    feat: add MyPropertiesRepo abstract class with getMyProperties method

---
+++ lib/Features/my_properties/data/repos/my_properties_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:stay_match/Features/my_properties/data/models/my_properties_response.dart';
+import 'package:stay_match/core/errors/failures.dart';
+
+abstract class MyPropertiesRepo{
+  Future<Either<Failure,MyPropertiesResponse>> getMyProperties({String? filter,int? page,int? pageSize});
+}
