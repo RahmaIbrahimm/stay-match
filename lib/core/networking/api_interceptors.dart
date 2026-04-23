9a0e98318f677ceba43f20ae2942daf026e21e49
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:22:35 +0200

    refactor: cleanup and reformat api_interceptors.dart

--- lib/core/networking/api_interceptors.dart
+++ lib/core/networking/api_interceptors.dart
@@ -1,121 +1,3 @@
-// import 'package:dio/dio.dart';
-// import 'package:flutter/material.dart';
-// import 'package:stay_match/core/networking/endpoints.dart';
-// import 'package:stay_match/core/utils/secure_storage_helper.dart';
-//
-// class ApiInterceptors extends Interceptor {
-//   final Dio dio;
-//   bool _isRefreshing = false;
-//   final List<(_CompleteCallback, ErrorCallback)> _pendingRequests = [];
-//
-//   ApiInterceptors(this.dio);
-//
-//   @override
-//   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
-//     final token = await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.tokenKey);
-//
-//     if (token != null) {
-//       options.headers['Authorization'] = 'Bearer $token';
-//     }
-//
-//     super.onRequest(options, handler);
-//   }
-//
-//   @override
-//   void onError(DioException err, ErrorInterceptorHandler handler) async {
-//     if (err.response?.statusCode == 401) {
-//       // Token expired, try to refresh
-//       await _refreshToken(err, handler);
-//     } else {
-//       handler.next(err);
-//     }
-//   }
-// // fixme : add force logout
-//   Future<void> _refreshToken(DioException originalError, ErrorInterceptorHandler handler) async {
-//     // If already refreshing, add to queue
-//     if (_isRefreshing) {
-//       _pendingRequests.add((handler, originalError));
-//       return;
-//     }
-//
-//     _isRefreshing = true;
-//
-//     try {
-//       final refreshToken = await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.refreshTokenKey);
-//
-//       if (refreshToken == null) {
-//         // No refresh token, force logout
-//         // _forceLogout();
-//         handler.reject(originalError);
-//         return;
-//       }
-//
-//       // Call refresh token API
-//       final response = await dio.post(
-//         Endpoints.refreshToken,
-//         data: {'refreshToken': refreshToken},
-//       );
-//
-//       if (response.statusCode == 200) {
-//         final newToken = response.data['token'];
-//         final newRefreshToken = response.data['refreshToken'];
-//
-//         // Save new tokens
-//         await SecureStorageHelper.addToSecureStorage(key: SecureStorageHelper.tokenKey,value: newToken);
-//         await SecureStorageHelper.addToSecureStorage(key: SecureStorageHelper.refreshTokenKey,value: newRefreshToken);
-//
-//         // Retry all pending requests with new token
-//         for (final pending in _pendingRequests) {
-//           final (callback, error) = pending;
-//           _retryRequest(error.requestOptions, callback);
-//         }
-//         _pendingRequests.clear();
-//
-//         // Retry the original request
-//         _retryRequest(originalError.requestOptions, handler);
-//       } else {
-//         // Refresh failed, force logout
-//         // _forceLogout();
-//         handler.reject(originalError);
-//       }
-//     } catch (e) {
-//       // _forceLogout();
-//       handler.reject(originalError);
-//     } finally {
-//       _isRefreshing = false;
-//     }
-//   }
-//
-//   Future<void> _retryRequest(RequestOptions requestOptions, ErrorInterceptorHandler handler) async {
-//     final newToken = await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.tokenKey);
-//
-//     if (newToken != null) {
-//       requestOptions.headers['Authorization'] = 'Bearer $newToken';
-//     }
-//
-//     try {
-//       final response = await dio.fetch(requestOptions);
-//       handler.resolve(response);
-//     } catch (e) {
-//       handler.reject(e as DioException);
-//     }
-//   }
-//
-//   // void _forceLogout() {
-//   //   // Clear tokens
-//   //   SecureStorageHelper.clearAll();
-//   //
-//   //   // Navigate to login (using a global navigator key or callback)
-//   //   // You can use a GlobalKey<NavigatorState> for this
-//   //   WidgetsBinding.instance.addPostFrameCallback((_) {
-//   //     // Navigate to login screen
-//   //     // navigatorKey.currentState?.pushNamedAndRemoveUntil('/login', (route) => false);
-//   //   });
-//   // }
-// }
-//
-// typedef _CompleteCallback = ErrorInterceptorHandler;
-// typedef ErrorCallback = DioException;
 
 import 'dart:developer';
 
@@ -128,16 +10,20 @@ class ApiInterceptors extends Interceptor {
   bool _isRefreshing = false;
 
   // Use a list of completers or a simple list of pending requests to retry
-  final List<
-      ({RequestOptions options, ErrorInterceptorHandler handler})> _pendingRequests = [
-  ];
+  final List<({RequestOptions options, ErrorInterceptorHandler handler})>
+  _pendingRequests = [];
 
   ApiInterceptors(this.dio);
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
-    log('onRequest',name: 'ApiInterceptors');
-    final token = await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.tokenKey);
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    log('onRequest', name: 'ApiInterceptors');
+    final token = await SecureStorageHelper.readFromSecureStorage(
+      key: SecureStorageHelper.tokenKey,
+    );
     if (token != null) {
       options.headers['Authorization'] = 'Bearer $token';
     }
@@ -165,18 +51,25 @@ class ApiInterceptors extends Interceptor {
     }
   }
 
-  Future<void> _refreshToken(DioException originalError, ErrorInterceptorHandler handler) async {
+  Future<void> _refreshToken(
+    DioException originalError,
+    ErrorInterceptorHandler handler,
+  ) async {
     // If already refreshing, queue this request
     if (_isRefreshing) {
-      _pendingRequests.add(
-          (options: originalError.requestOptions, handler: handler));
+      _pendingRequests.add((
+        options: originalError.requestOptions,
+        handler: handler,
+      ));
       return;
     }
 
     _isRefreshing = true;
 
     try {
-      final refreshToken = await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.refreshTokenKey);
+      final refreshToken = await SecureStorageHelper.readFromSecureStorage(
+        key: SecureStorageHelper.refreshTokenKey,
+      );
       log('refresh token : $refreshToken', name: 'ApiInterceptors');
       if (refreshToken == null) {
         log('no refresh token', name: 'ApiInterceptors');
@@ -198,9 +91,13 @@ class ApiInterceptors extends Interceptor {
 
         // FIXED: Save to the correct keys
         await SecureStorageHelper.addToSecureStorage(
-            key: SecureStorageHelper.tokenKey, value: newToken);
+          key: SecureStorageHelper.tokenKey,
+          value: newToken,
+        );
         await SecureStorageHelper.addToSecureStorage(
-            key: SecureStorageHelper.refreshTokenKey, value: newRefreshToken);
+          key: SecureStorageHelper.refreshTokenKey,
+          value: newRefreshToken,
+        );
 
         // 1. Retry the original request that triggered the refresh
         await _retryRequest(originalError.requestOptions, handler);
@@ -222,8 +119,13 @@ class ApiInterceptors extends Interceptor {
     }
   }
 
-  Future<void> _retryRequest(RequestOptions requestOptions, ErrorInterceptorHandler handler) async {
-    final newToken = await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.tokenKey);
+  Future<void> _retryRequest(
+    RequestOptions requestOptions,
+    ErrorInterceptorHandler handler,
+  ) async {
+    final newToken = await SecureStorageHelper.readFromSecureStorage(
+      key: SecureStorageHelper.tokenKey,
+    );
 
     if (newToken != null) {
       requestOptions.headers['Authorization'] = 'Bearer $newToken';
