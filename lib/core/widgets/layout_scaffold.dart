801e23ad8566c9f34196fa92b06b9019297746a6
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:34:41 +0200

    style: fix formatting in layout_scaffold.dart

--- lib/core/widgets/layout_scaffold.dart
+++ lib/core/widgets/layout_scaffold.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:stay_match/core/widgets/nav_bar.dart';
+
 class LayoutScaffold extends StatelessWidget {
   const LayoutScaffold({super.key, required this.navigationShell});
   final StatefulNavigationShell navigationShell;
