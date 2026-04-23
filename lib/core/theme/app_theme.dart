138b1671f8245632e71f0787750a93c0dc53df20
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:23:53 +0200

    style: format app_theme.dart for better readability

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,7 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 
 abstract class AppTheme {
-  static final theme = ThemeData(
-    scaffoldBackgroundColor: Colors.white
-  );
+  static final theme = ThemeData(scaffoldBackgroundColor: Colors.white);
 }
