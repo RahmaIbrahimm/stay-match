7d5b003822af317b87f143d070b1973d55696e0b
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:44:51 +0200

    feat: add chat, property management, and profile API endpoints in Endpoints class

--- lib/core/networking/endpoints.dart
+++ lib/core/networking/endpoints.dart
@@ -12,10 +12,25 @@ abstract class Endpoints {
   static const String getAllApartments = 'api/Property';
   static const String getApartmentDetails = 'api/Property/';
   static const String refreshToken = 'api/Account/refresh-token';
+  // chat
+  static const String myChats = 'api/Chat/mychats';
+  static const String startChat = 'api/Chat/start';
+  static const String markRead = 'api/Chat/mark-as-read';
+  static const String sendMessage = 'api/Chat/send';
+  // add Property
+  static const String addApartment = 'api/Property/AddEntireProperty';
+  static const String addRoom = 'api/Property/AddSharedProperty';
+  static const String uploadImage = 'api/Property/upload-image';
+  static const String myProperties = 'api/Property/MyProperty';
+  static const String profileData = 'api/UserProfile';
+  static const String updateProfile = 'api/UserProfile';
 
   // client ids uses for google login
-  static const String webClientId= '936135595361-tkjd357n4h18pd6pc4pfoch6rto0vlh5.apps.googleusercontent.com';
-  static const String androidClientId= '936135595361-h3462ve7821dq8hcqamq5f3i632qruf9.apps.googleusercontent.com';
+  static const String webClientId =
+      '936135595361-tkjd357n4h18pd6pc4pfoch6rto0vlh5.apps.googleusercontent.com';
+  static const String androidClientId =
+      '936135595361-h3462ve7821dq8hcqamq5f3i632qruf9.apps.googleusercontent.com';
+  static const String chatApiKey = '3dumrx9thsgm';
   static const String mapsApiKey = 'AIzaSyBwB2ppYr6L3eXhGoRaql8iNdxIjTzvT3g';
   static const String placesApiKey = 'AIzaSyD3WRKYi-q6lImsJKkUcZ5uU07vfYEWq2M';
 }
