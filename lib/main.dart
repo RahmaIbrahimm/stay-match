08c75d89cc54b2312f2b7939d0460cc84634b172
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 18:43:19 +0200

    style: format imports and code structure in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,20 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stay_match/Features/auth/data/repos/auth_repo_impl.dart';
 import 'package:stay_match/core/theme/app_theme.dart';
 import 'package:stay_match/core/utils/service_locator.dart';
-import 'package:stay_match/Features/auth/data/repos/auth_repo_impl.dart';
 
+import 'Features/auth/manager/auth_cubit.dart';
 import 'core/routing/app_routing.dart';
 import 'core/utils/secure_storage_helper.dart';
-import 'Features/auth/manager/auth_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
-  await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.tokenKey);
+  await SecureStorageHelper.readFromSecureStorage(
+    key: SecureStorageHelper.tokenKey,
+  );
   runApp(
     MultiBlocProvider(
       providers: [
@@ -30,7 +32,7 @@ class StayMatch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(402 , 874),
+      designSize: Size(402, 874),
       child: DevicePreview(
         enabled: true,
         builder: (context) => MaterialApp.router(
