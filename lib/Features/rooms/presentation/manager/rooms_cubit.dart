582bc0ce7ae100ec2b86aecd08e55d3d547416b7
Rahma <rahmaibrahim315@gmail.com>
2026-04-23 17:33:05 +0200

    fix: refactor getAllRooms method to include forceRefresh parameter and simplify logging

--- lib/Features/rooms/presentation/manager/rooms_cubit.dart
+++ lib/Features/rooms/presentation/manager/rooms_cubit.dart
@@ -26,50 +26,29 @@ class RoomsCubit extends Cubit<RoomsState> {
     }
   }
 
-  Future<void> getAllRooms() async {
-    log(
-      'before : the token stored is: ${ await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.refreshTokenKey) ?? '--------> NO TOKEN WTFFFF'}',
-    );
+  Future<void> getAllRooms({bool forceRefresh = false}) async {
     emit(GetRoomsLoading());
-    log('1.Started loading');
-    // todo: search / filter impl
     var response = await roomsRepo.getAllRooms();
-
     response.fold(
       (fail) {
-        log('Failure object: $fail');
-        log('Failure type: ${fail.runtimeType}');
-        log('Failure errMessage: ${fail.errMessage}');
-
-        // Verify what we're emitting
-        final errorMessage = fail.errMessage;
-        log('Emitting error message: $errorMessage');
-
         emit(GetRoomsFailure(errMessage: fail.errMessage));
         log('2.error happened getting rooms');
       },
       (response) async {
         if (response.isSuccess == true) {
           emit(GetRoomsSuccess(response: response));
-          log('2.SUCCESS');
-          log(
-            'after : the token stored is: ${await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.refreshTokenKey) ?? '--------> NO TOKEN WTFFFF'}',
-          );
         } else {
-          log(
-            'after 2 : the token stored is: ${await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.refreshTokenKey) ?? '--------> NO TOKEN WTFFFF'}',
-          );
           emit(
             GetRoomsFailure(
               errMessage: response.message ?? 'Error getting Rooms',
             ),
           );
-          log('3.error');
-          log(
-            'after : the token stored is: ${await SecureStorageHelper.readFromSecureStorage(key: SecureStorageHelper.refreshTokenKey) ?? '--------> NO TOKEN WTFFFF'}',
-          );
         }
       },
     );
+  }
+
+  Future<void> refreshRooms() async {
+    await getAllRooms(forceRefresh: true);
   }
 }
