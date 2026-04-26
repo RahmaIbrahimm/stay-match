8f3e87958cc19b275d239155f5020786c43f0fc6
Rahma <rahmaibrahim315@gmail.com>
2026-04-26 04:11:59 +0300

    feat: uncomment and refactor MessagesView to use MessageCubit and MessageInputBar

--- lib/Features/chat/presentation/views/messages_view.dart
+++ lib/Features/chat/presentation/views/messages_view.dart
@@ -1,44 +1,38 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:stay_match/Features/chat/data/repos/chat_repo.dart';
-// import 'package:stay_match/core/utils/service_locator.dart';
-//
-// import '../../data/repos/chat_repo_impl.dart';
-// import '../manager/message_cubit.dart';
-// import '../widgets/messages_view/messages_view_body.dart';
-//
-// class MessagesView extends StatefulWidget {
-//   final String otherUserId;
-//
-//   const MessagesView({super.key, required this.otherUserId});
-//
-//   @override
-//   State<MessagesView> createState() => _MessagesViewState();
-// }
-//
-// class _MessagesViewState extends State<MessagesView> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     // أول ما نفتح، بننادي الـ startChat عشان نجيب القديم ونفتح الـ SignalR
-//     context.read<MessageCubit>().startChat(otherUserId: widget.otherUserId);
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocProvider(
-//       create: (context) => MessageCubit(chatRepo: getIt.get<ChatRepoImpl>(), hubConnection: hubConnection),
-//       child: Scaffold(
-//         appBar: AppBar(
-//           title: const Text("Chat"), // ممكن تجيب الاسم من الـ State لاحقاً
-//           leading: IconButton(
-//             icon: const Icon(Icons.arrow_back_ios),
-//             onPressed: () => context.pop(),
-//           ),
-//         ),
-//         body: const MessagesViewBody(),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stay_match/core/networking/chat_service.dart';
+import 'package:stay_match/core/utils/service_locator.dart';
+
+import '../../data/repos/chat_repo_impl.dart';
+import '../manager/message_cubit.dart';
+import '../widgets/messages_view/message_input_bar.dart';
+import '../widgets/messages_view/messages_view_body.dart';
+
+// --- 1. Main View Entry Point ---
+class MessagesView extends StatelessWidget {
+  final String otherUserId;
+
+  const MessagesView({super.key, required this.otherUserId});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => MessageCubit(
+        chatRepo: getIt.get<ChatRepoImpl>(),
+        chatService: getIt.get<ChatService>(),
+        otherUserId: otherUserId,
+      )..startChat(otherUserId: otherUserId),
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: const MessagesViewBody(),
+            resizeToAvoidBottomInset: true,
+            bottomNavigationBar: MessageInputBar(
+              chatId: BlocProvider.of<MessageCubit>(context).chatId ?? -1,
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
