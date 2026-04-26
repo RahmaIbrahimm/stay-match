ab24683c11975cc6a74e6d8873262e1b6bcd4a06
Rahma <rahmaibrahim315@gmail.com>
2026-04-26 04:13:12 +0300

    feat: add profile picture management and account deletion methods to ProfileRepo

--- lib/Features/profile/data/repos/profile_repo.dart
+++ lib/Features/profile/data/repos/profile_repo.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:stay_match/Features/profile/data/models/delete_account_response.dart';
 import 'package:stay_match/Features/profile/data/models/profile_response.dart';
 import 'package:stay_match/Features/profile/data/models/update_profile_request.dart';
+import 'package:stay_match/Features/profile/data/models/upload_profile_picture_response.dart';
 import 'package:stay_match/core/errors/failures.dart';
 
 import '../models/update_profile_response.dart';
@@ -9,4 +11,13 @@ abstract class ProfileRepo {
   Future<Either<Failure,ProfileResponse>> getProfile();
   Future<Either<Failure,UpdateProfileResponse>> updateProfile(
       {required UpdateProfileRequest request});
+
+  Future<Either<Failure, UploadProfilePictureResponse>> uploadProfileImg({
+    required String imageFile,
+  });
+
+  Future<Either<Failure, UploadProfilePictureResponse>> updateProfileImg({
+    required String imageFile,
+  });
+  Future<Either<Failure,DeleteAccountResponse>> deleteAccount();
 }
