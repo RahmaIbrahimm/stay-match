347cc3ee911c2e66aeff8ef3e2a67633fc587d9a
Rahma <rahmaibrahim315@gmail.com>
2026-04-30 03:26:21 +0300

    refactor: move chat_helper.dart to shared widgets and add date grouping logic

--- lib/Features/chat/presentation/widgets/shared/chat_helper.dart
+++ lib/Features/chat/presentation/widgets/shared/chat_helper.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 
+import '../../../data/models/start_chat_response.dart';
+
 class ChatHelper {
   static Color _getAvatarColor(String name) {
     final List<Color> colors = [
@@ -80,4 +82,39 @@ class ChatHelper {
     } catch (e) {
       return "";
     }
-  }}
+  }
+
+  static String getGroupDate(DateTime date) {
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    final yesterday = today.subtract(const Duration(days: 1));
+    final aWeekAgo = today.subtract(const Duration(days: 7));
+
+    final msgDate = DateTime(date.year, date.month, date.day);
+
+    if (msgDate == today) {
+      return "TODAY";
+    } else if (msgDate == yesterday) {
+      return "YESTERDAY";
+    } else if (msgDate.isAfter(aWeekAgo)) {
+      // Returns Monday, Tuesday, etc.
+      return DateFormat('EEEE').format(date).toUpperCase();
+    } else {
+      // Returns 25 APR 2026
+      return DateFormat('dd MMM yyyy').format(date).toUpperCase();
+    }
+  }
+
+  static Map<String, List<Messages>> groupMessagesByDate(
+      List<Messages> messages) {
+    Map<String, List<Messages>> groups = {};
+    for (var message in messages) {
+      String dateKey = getGroupDate(DateTime.parse(message.sentAt!));
+      if (!groups.containsKey(dateKey)) {
+        groups[dateKey] = [];
+      }
+      groups[dateKey]!.add(message);
+    }
+    return groups;
+  }
+}
