f3d427bed5c10ea299061df8a88d1240cc749606
Rahma <rahmaibrahim315@gmail.com>
2026-04-30 03:24:18 +0300

    feat: add RestartWidget for programmatically restarting the app subtree

---
+++ lib/core/widgets/restart_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+class RestartWidget extends StatefulWidget {
+  final Widget child;
+  const RestartWidget({super.key, required this.child});
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_RestartWidgetState>()?.restart();
+  }
+
+  @override
+  State<RestartWidget> createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
+
+  void restart() {
+    setState(() {
+      key = UniqueKey(); // This kills the old tree and starts a new one
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child,
+    );
+  }
+}
