de3e3b06a729d1165a38e6806c6869782397ad62
Rahma <rahmaibrahim315@gmail.com>
2026-05-17 07:15:37 +0300

    refactor: update `CustomToggleSwitch` to support optional callbacks and `onChangedBool` function

--- lib/core/widgets/custom_toggle_switch.dart
+++ lib/core/widgets/custom_toggle_switch.dart
@@ -1,62 +1,23 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stay_match/Features/filter/presentation/widgets/filter_helper.dart';
 import 'package:stay_match/core/constants/app_colors.dart';
 
 class CustomToggleSwitch extends StatefulWidget {
   const CustomToggleSwitch({
     super.key,
-    required this.onTap,
-    required this.current, this.height, this.width,
+    this.onTap,
+    required this.current, this.height, this.width, this.onChangedBool,
   });
 
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
+  final OnChangedBool? onChangedBool;
   final bool current;
   final double? height;
   final double? width;
   @override
   State<CustomToggleSwitch> createState() => _CustomToggleSwitchState();
 }
-
-// class _CustomToggleSwitchState extends State<CustomToggleSwitch> {
-//   late bool isOn = widget.current;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: () {
-//         setState(() {
-//           log('${widget.current}');
-//           isOn = !isOn;
-//         });
-//       },
-//       child: AnimatedContainer(
-//         duration: Duration(milliseconds: 300),
-//         width: 50,
-//         height: 28,
-//         padding: EdgeInsets.all(4),
-//         decoration: BoxDecoration(
-//           borderRadius: BorderRadius.circular(20),
-//           color: isOn ? AppColors.primary : Colors.grey,
-//         ),
-//         child: AnimatedAlign(
-//           duration: Duration(milliseconds: 200),
-//           alignment: isOn ? Alignment.centerRight : Alignment.centerLeft,
-//           child: Container(
-//             width:widget.width?? 20,
-//             height:widget.height?? 25,
-//             decoration: BoxDecoration(
-//               shape: BoxShape.circle,
-//               color: AppColors.containerColor,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class _CustomToggleSwitchState extends State<CustomToggleSwitch> {
   late bool isOn;
 
@@ -88,7 +49,8 @@ class _CustomToggleSwitchState extends State<CustomToggleSwitch> {
         });
         // 3. CRITICAL: We call the callback so the Cubit actually updates.
         // Without this line, the 'Yes/No' text in your Row won't change.
-        widget.onTap();
+        if(widget.onTap != null) widget.onTap!();
+        if(widget.onChangedBool != null) widget.onChangedBool!(isOn);
       },
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 300),
