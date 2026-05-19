1c5452a8bacf130f0b96504e88c1f62fab342bc2
Rahma <rahmaibrahim315@gmail.com>
2026-05-19 11:28:11 +0300

    feat: implement myBookingRequests in BookingRepo and BookingRepoImpl

--- lib/Features/booking/data/repos/booking_repo_impl.dart
+++ lib/Features/booking/data/repos/booking_repo_impl.dart
@@ -2,13 +2,13 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:stay_match/Features/booking/data/model/apartment_booking_request_request.dart';
 import 'package:stay_match/Features/booking/data/model/apartment_booking_request_response.dart';
+import 'package:stay_match/Features/booking/data/model/my_bookings_response.dart';
 import 'package:stay_match/Features/booking/data/model/room_booking_request_request.dart';
 import 'package:stay_match/Features/booking/data/repos/booking_repo.dart';
 import 'package:stay_match/core/errors/failures.dart';
 import 'package:stay_match/core/networking/api_service.dart';
 
 import '../../../../core/networking/endpoints.dart';
-import '../../../saved/data/models/recommended_properties_response.dart';
 import '../model/room_booking_request_response.dart';
 
 class BookingRepoImpl extends BookingRepo {
@@ -45,6 +45,29 @@ class BookingRepoImpl extends BookingRepo {
       );
 
       return Right(RoomBookingRequestResponse.fromJson(response));
+    } on DioException catch (e) {
+      // Handle specific Dio/Server errors here
+      return Left(ServerFailure.fromDioError(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, MyBookingsResponse>> myBookingRequests(
+      {String? location, int? year, int? month, int? day, int? page, int? pageSize}) async {
+    try {
+      // We pass the serialized JSON directly to the data body
+      final response = await apiService.get(
+        Endpoints.myBookingRequests,
+        queryParameters: {
+          'location': location,
+          'year': year,
+          'month': month,
+          'day': day,
+          'page': page,
+          'pageSize': pageSize,
+        },);
+
+      return Right(MyBookingsResponse.fromJson(response));
     } on DioException catch (e) {
       // Handle specific Dio/Server errors here
       return Left(ServerFailure.fromDioError(e));
