dbc972b2c62cdcdb18f748f91cbc4cb0d9122b9a
Rahma <rahmaibrahim315@gmail.com>
2026-05-23 23:23:43 +0300

    feat: add ReviewsState and its subclasses for managing reviews state
    - Create `ReviewsState` sealed class and its variants: `ReviewsInitial`, `ReviewsLoading`, `ReviewsSuccess`, and `ReviewsFailure`.
    - Implement `ReviewsSuccess` to hold `GetApartmentReviews` data.
    - Implement `ReviewsFailure` to hold error messages.

---
+++ lib/Features/reviews/presentation/manager/reviews_state.dart
@@ -0,0 +1,33 @@
+part of 'reviews_cubit.dart';
+
+sealed class ReviewsState extends Equatable {
+  const ReviewsState();
+}
+
+final class ReviewsInitial extends ReviewsState {
+  @override
+  List<Object> get props => [];
+}
+
+final class ReviewsLoading extends ReviewsState {
+  @override
+  List<Object> get props => [];
+}
+
+final class ReviewsSuccess extends ReviewsState {
+  final GetApartmentReviews? apartmentReviews;
+
+  const ReviewsSuccess({this.apartmentReviews});
+
+  @override
+  List<Object> get props => [?apartmentReviews];
+}
+
+final class ReviewsFailure extends ReviewsState {
+  final String errMessage;
+
+  const ReviewsFailure({required this.errMessage});
+
+  @override
+  List<Object> get props => [errMessage];
+}
