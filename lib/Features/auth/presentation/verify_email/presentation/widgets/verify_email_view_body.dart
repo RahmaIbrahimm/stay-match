af76bdac5e38c842def6668ba4ed7534c0c96eaa
Rahma <rahmaibrahim315@gmail.com>
2026-06-11 01:42:30 +0300

    feat: update email verification flow and refactor directory structure
    - Refactor folder structure for email verification widgets.
    - Implement `VerifyEmailContainerBody` with improved focus management and navigation logic.
    - Add a local `GlobalKey<FormState>` for the verification form.
    - Enhance `VerifyEmailView` by adding a transparent `AppBar` with a back button.
    - Update resend code logic to include the `isResend` flag in `AuthCubit`.

--- lib/Features/auth/presentation/verify_email/presentation/widgets/verify_email_view_body.dart
+++ lib/Features/auth/presentation/verify_email/presentation/widgets/verify_email_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:stay_match/Features/auth/presentation/verify_email/presentation/views/widgets/verify_email_container_body.dart';
+import 'package:stay_match/Features/auth/presentation/verify_email/presentation/widgets/verify_email_container_body.dart';
 
-import '../../../../widgets/custom_container.dart';
-import '../../../../widgets/gradient_background.dart';
+import '../../../widgets/custom_container.dart';
+import '../../../widgets/gradient_background.dart';
 
 class VerifyEmailViewBody extends StatelessWidget {
   const VerifyEmailViewBody({super.key});
