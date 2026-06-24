dbbfdc345e330ef84143b64a0189cc9727284977
Rahma <rahmaibrahim315@gmail.com>
2026-06-24 11:55:19 +0300

    feat: add DraggableChatbotFab to LayoutScaffold
    - Import `DraggableChatbotFab` widget.
    - Wrap `navigationShell` within `DraggableChatbotFab` in the `Scaffold` body to enable the floating chatbot button across the main layout.

--- lib/core/widgets/layout_scaffold.dart
+++ lib/core/widgets/layout_scaffold.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:stay_match/core/widgets/nav_bar.dart';
 
+import 'draggable_chatbot_fab.dart';
+
 class LayoutScaffold extends StatelessWidget {
   const LayoutScaffold({super.key, required this.navigationShell});
   final StatefulNavigationShell navigationShell;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: navigationShell,
+      body: DraggableChatbotFab(child: navigationShell),
       bottomNavigationBar: NavBar(navigationShell: navigationShell),
     );
   }
