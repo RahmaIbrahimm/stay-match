ebf16547b881a1c3baf69ef6604f06260274e754
Rahma <rahmaibrahim315@gmail.com>
2026-06-26 14:10:51 +0300

    feat: add room reviews functionality and host reply feature in ReviewsRepo

--- lib/Features/reviews/data/repos/reviews_repo.dart
+++ lib/Features/reviews/data/repos/reviews_repo.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:stay_match/Features/reviews/data/models/get_apartment_reviews.dart';
+import 'package:stay_match/Features/reviews/data/models/host_reply_response.dart';
 import 'package:stay_match/Features/reviews/data/models/review_recommendations.dart';
 import 'package:stay_match/Features/reviews/data/models/write_review_response.dart';
 import 'package:stay_match/core/errors/failures.dart';
@@ -7,8 +8,15 @@ import 'package:stay_match/core/errors/failures.dart';
 import '../models/write_review_request.dart';
 
 abstract class ReviewsRepo {
-  Future<Either<Failure, GetApartmentReviews>> getApartmentReviews({
+  Future<Either<Failure, GetPropertyReviews>> getApartmentReviews({
     required int propertyId,
+    int? page,
+    int? pageSize,
+    String? sortBy= 'All',
+    String? search,
+  });
+  Future<Either<Failure, GetPropertyReviews>> getRoomReviews({
+    required int roomId,
     int? page,
     int? pageSize,
     String? sortBy= 'All',
@@ -17,5 +25,6 @@ abstract class ReviewsRepo {
 
   Future<Either<Failure, WriteReviewResponse>> writeReview({required WriteReviewRequest request});
   Future<Either<Failure, ReviewRecommendations>> getRecommendations();
+  Future<Either<Failure, HostReplyResponse>> hostReplyToReview({required int reviewId, required String response});
 
 }
