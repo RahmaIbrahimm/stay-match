6da4d56b1b6d3c5b3529d284957b2869467a7b14
Rahma <rahmaibrahim315@gmail.com>
2026-06-26 14:21:04 +0300

    feat: add FeatureItem class and refactor no_properties_sliver for improved readability

---
+++ lib/Features/rooms/presentation/widgets/room_details/feature_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FeatureItem {
+  final IconData icon;
+  final String label;
+
+  const FeatureItem(this.icon, this.label);
+}
--- lib/Features/shared/widgets/no_properties_sliver.dart
+++ lib/Features/shared/widgets/no_properties_sliver.dart
@@ -10,52 +10,50 @@ class NoPropertiesSliver extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Center(
-        child: RPadding(
-          padding: EdgeInsets.all(32.0.r),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.house_outlined,
-                size: 90.sp,
-                color: AppColors.primary.withValues(alpha: 0.5),
+    return Center(
+      child: RPadding(
+        padding: EdgeInsets.all(32.0.r),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.house_outlined,
+              size: 90.sp,
+              color: AppColors.primary.withValues(alpha: 0.5),
+            ),
+            SizedBox(height: 16.h),
+            Text(
+              AppStrings.noApartmentsAvailable,
+              style: AppStyles.bold20poppins.copyWith(
+                color: AppColors.textColorPrimary,
               ),
-              SizedBox(height: 16.h),
-              Text(
-                AppStrings.noApartmentsAvailable,
-                style: AppStyles.bold20poppins.copyWith(
-                  color: AppColors.textColorPrimary,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              'At the Moment',
+              style: AppStyles.bold20poppins.copyWith(
+                color: AppColors.textColorSecondary,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(height: 16.h),
+            Container(
+              padding: EdgeInsets.all(12.r),
+              margin: EdgeInsets.symmetric(horizontal: 20.r),
+              decoration: BoxDecoration(
+                color: AppColors.primary.withValues(alpha: 0.1),
+                borderRadius: BorderRadius.circular(15.r),
+              ),
+              child: Text(
+                'Please check again later.\nNew apartments are added regularly!',
+                style: AppStyles.regular14poppins.copyWith(
+                  color: AppColors.primary,
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 8.h),
-              Text(
-                'At the Moment',
-                style: AppStyles.bold20poppins.copyWith(
-                  color: AppColors.textColorSecondary,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(height: 16.h),
-              Container(
-                padding: EdgeInsets.all(12.r),
-                margin: EdgeInsets.symmetric(horizontal: 20.r),
-                decoration: BoxDecoration(
-                  color: AppColors.primary.withValues(alpha: 0.1),
-                  borderRadius: BorderRadius.circular(15.r),
-                ),
-                child: Text(
-                  'Please check again later.\nNew apartments are added regularly!',
-                  style: AppStyles.regular14poppins.copyWith(
-                    color: AppColors.primary,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
